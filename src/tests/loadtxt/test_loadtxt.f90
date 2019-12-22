22a6dc6f0bf8cc766b7bd13679437a166da83aed
Vandenplas, Jeremie <jeremie.vandenplas@gmail.com>
2019-12-22 12:20:01 -0700

    Single and quadruple precisions for load/savetxt

--- src/tests/loadtxt/test_loadtxt.f90
+++ src/tests/loadtxt/test_loadtxt.f90
@@ -1,9 +1,15 @@
 program test_loadtxt
-use iso_fortran_env, only: dp=>real64
+use iso_fortran_env, only: sp=>real32, dp=>real64 ,qp=>real128
 use stdlib_experimental_io, only: loadtxt
 implicit none
 
+real(sp), allocatable :: s(:, :)
 real(dp), allocatable :: d(:, :)
+real(qp), allocatable :: q(:, :)
+
+call loadtxt("array1.dat", s)
+call print_array(s)
+
 call loadtxt("array1.dat", d)
 call print_array(d)
 
@@ -16,15 +22,34 @@ call print_array(d)
 call loadtxt("array4.dat", d)
 call print_array(d)
 
+call loadtxt("array4.dat", q)
+call print_array(q)
+
 contains
 
 subroutine print_array(a)
-real(dp) :: a(:, :)
+class(*),intent(in) :: a(:, :)
 integer :: i
 print *, "Array, shape=(", size(a, 1), ",", size(a, 2), ")"
-do i = 1, size(a, 1)
+
+ select type(a)
+  type is(real(sp))
+   do i = 1, size(a, 1)
     print *, a(i, :)
-end do
+   end do
+  type is(real(dp))
+   do i = 1, size(a, 1)
+    print *, a(i, :)
+   end do
+  type is(real(qp))
+   do i = 1, size(a, 1)
+    print *, a(i, :)
+   end do
+  class default
+   write(*,'(a)')'The proposed type is not supported'
+   error stop
+ end select
+
 end subroutine
 
 end program
--- src/tests/loadtxt/test_savetxt.f90
+++ src/tests/loadtxt/test_savetxt.f90
@@ -1,11 +1,12 @@
 program test_loadtxt
-use iso_fortran_env, only: sp=>real32, dp=>real64
+use iso_fortran_env, only: sp=>real32, dp=>real64 ,qp=>real128
 use stdlib_experimental_io, only: loadtxt, savetxt
 use stdlib_experimental_error, only: assert
 implicit none
 
 call test_sp()
 call test_dp()
+call test_qp()
 
 contains
 
@@ -40,6 +41,22 @@ contains
     call loadtxt("tmp.dat", d2)
     call assert(all(shape(d2) == [2, 3]))
     call assert(all(abs(e-d2) < epsilon(1._dp)))
+    end subroutine
+
+    subroutine test_qp()
+    real(qp) :: d(3, 2), e(2, 3)
+    real(qp), allocatable :: d2(:, :)
+    d = reshape([1, 2, 3, 4, 5, 6], [3, 2])
+    call savetxt("tmp.dat", d)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [3, 2]))
+    call assert(all(abs(d-d2) < epsilon(1._qp)))
+
+    e = reshape([1, 2, 3, 4, 5, 6], [2, 3])
+    call savetxt("tmp.dat", e)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [2, 3]))
+    call assert(all(abs(e-d2) < epsilon(1._qp)))
     end subroutine
 
 end program
