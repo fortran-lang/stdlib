2375321ab4354c68bc5c0bafa8112972ba5c6354
Ondřej Čertík <ondrej@certik.us>
2020-01-02 10:06:25 -0700

    Update src/stdlib_experimental_kinds.f90
    Co-Authored-By: Jeremie Vandenplas <jeremie.vandenplas@gmail.com>

--- src/stdlib_experimental_kinds.f90
+++ src/stdlib_experimental_kinds.f90
@@ -3,7 +3,7 @@ module stdlib_experimental_kinds
 !use iso_fortran_env, only: sp=>real32, dp=>real64, qp=>real128
 !use iso_fortran_env, only: int32, int64, int128
 use iso_c_binding, only: sp=>c_float, dp=>c_double, qp=>c_float128
-use iso_c_binding, only: int32=>c_int32_t, int64=>c_int64_t, int128=>c_int128_t
+use iso_c_binding, only: int8=>c_int8_t, int16=>c_int16_t, int32=>c_int32_t, int64=>c_int64_t
 implicit none
 private
 public sp, dp, qp, int32, int64, int128
