6a263ffa40ae122c9e074de170eecbf47775d74b
Ondřej Čertík <ondrej@certik.us>
2020-01-03 09:05:02 -0700

    Add a test for "open"

---
+++ src/tests/io/test_open.f90
@@ -0,0 +1,49 @@
+program test_open
+use stdlib_experimental_io, only: open
+use stdlib_experimental_error, only: assert
+implicit none
+
+character(:), allocatable :: filename
+integer :: u, a(3)
+
+filename = get_outpath() // "/io_open.dat"
+
+! Test mode "w"
+u = open(filename, "w")
+write(u, *) 1, 2, 3
+close(u)
+
+! Test mode "r"
+u = open(filename, "r")
+read(u, *) a
+call assert(all(a == [1, 2, 3]))
+close(u)
+
+! Test mode "a"
+u = open(filename, "a")
+write(u, *) 4, 5, 6
+close(u)
+u = open(filename, "r")
+read(u, *) a
+call assert(all(a == [1, 2, 3]))
+read(u, *) a
+call assert(all(a == [4, 5, 6]))
+close(u)
+
+
+contains
+
+    function get_outpath() result(outpath)
+    integer :: ierr
+    character(256) :: argv
+    character(:), allocatable :: outpath
+
+    call get_command_argument(1, argv, status=ierr)
+    if (ierr==0) then
+        outpath = trim(argv)
+    else
+        outpath = '.'
+    endif
+    end function get_outpath
+
+end program
