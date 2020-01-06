f0a68868fb84880f5e3e6f2173bf371df313f22a
Ondřej Čertík <ondrej@certik.us>
2020-01-06 07:56:18 -0700

    Update src/stdlib_experimental_io.f90
    Co-Authored-By: Jeremie Vandenplas <jeremie.vandenplas@gmail.com>

--- src/stdlib_experimental_io.f90
+++ src/stdlib_experimental_io.f90
@@ -336,7 +336,7 @@ end select
 select case (mode_(3:3))
 case('t')
     form_='formatted'
-case('b', 's')
+case('b')
     form_='unformatted'
 case default
     call error_stop("Unsupported mode: "//mode_(3:3))
