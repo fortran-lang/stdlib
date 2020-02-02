c1a795688cedd3bfdbd40fc88819285560afdf89
Juan Fiol <juanfiol@gmail.com>
2020-02-02 10:57:50 -0300

    Added support and tests for complex numbers in loadtxt, savetxt

--- src/tests/io/test_savetxt_qp.f90
+++ src/tests/io/test_savetxt_qp.f90
@@ -1,31 +1,32 @@
 program test_savetxt_qp
-use stdlib_experimental_kinds, only: qp
-use stdlib_experimental_io, only: loadtxt, savetxt
-use stdlib_experimental_error, only: assert
-implicit none
+  use stdlib_experimental_kinds, only: qp
+  use stdlib_experimental_io, only: loadtxt, savetxt
+  use stdlib_experimental_error, only: assert
+  implicit none
 
-character(:), allocatable :: outpath
+  character(:), allocatable :: outpath
 
-outpath = get_outpath() // "/tmp_qp.dat"
+  outpath = get_outpath() // "/tmp_qp.dat"
 
-call test_qp(outpath)
+  call test_qp(outpath)
+  call test_cqp(outpath)
 
 contains
 
-    function get_outpath() result(outpath)
+  function get_outpath() result(outpath)
     integer :: ierr
     character(256) :: argv
     character(:), allocatable :: outpath
 
     call get_command_argument(1, argv, status=ierr)
     if (ierr==0) then
-        outpath = trim(argv)
+      outpath = trim(argv)
     else
-        outpath = '.'
+      outpath = '.'
     endif
-    end function get_outpath
+  end function get_outpath
 
-    subroutine test_qp(outpath)
+  subroutine test_qp(outpath)
     character(*), intent(in) :: outpath
     real(qp) :: d(3, 2), e(2, 3)
     real(qp), allocatable :: d2(:, :)
@@ -40,6 +41,25 @@ contains
     call loadtxt(outpath, d2)
     call assert(all(shape(d2) == [2, 3]))
     call assert(all(abs(e-d2) < epsilon(1._qp)))
-    end subroutine
+  end subroutine test_qp
 
-end program
+
+  subroutine test_cqp(outpath)
+    character(*), intent(in) :: outpath
+    complex(qp) :: d(3, 2), e(2, 3)
+    complex(qp), allocatable :: d2(:, :)
+    d = reshape([1, 2, 3, 4, 5, 6], [3, 2])
+    call savetxt(outpath, d)
+    call loadtxt(outpath, d2)
+    call assert(all(shape(d2) == [3, 2]))
+    call assert(all(abs(d-d2) < epsilon(1._qp)))
+
+    e = reshape([1, 2, 3, 4, 5, 6], [2, 3])
+    call savetxt(outpath, e)
+    call loadtxt(outpath, d2)
+    call assert(all(shape(d2) == [2, 3]))
+    call assert(all(abs(e-d2) < epsilon(1._qp)))
+  end subroutine test_cqp
+
+
+end program test_savetxt_qp
