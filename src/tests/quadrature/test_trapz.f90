d1c91ecc56f244176d0fe386eb71dd6bdfdbae92
Nathaniel Shaffer <nrshaffer@protonmail.com>
2020-02-07 06:35:52 -0700

    replace f.p. equality checks with tolerance checks

--- src/tests/quadrature/test_trapz.f90
+++ src/tests/quadrature/test_trapz.f90
@@ -12,7 +12,7 @@ program test_trapz
     call test_trapz_weights_sp
     call test_trapz_weights_dp
     call test_trapz_weights_qp
-    
+
     call test_trapz_zero_sp
     call test_trapz_zero_dp
     call test_trapz_zero_qp
@@ -28,26 +28,26 @@ contains
         integer :: i
 
         print *, "test_trapz_sp"
-        
+
         y = [(real(i-1, sp), i = 1, n)]
 
         val = trapz(y, 1.0_sp)
         ans = 128.0_sp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         val = trapz(y, 0.5_sp)
         ans = 64.0_sp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = [((i-1)*4.0_sp/real(n-1, sp), i = 1, n)]
         val = trapz(y, x)
         ans = 32.0_sp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         val = trapz(y, x)
         ans = 2728.0_sp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
     end subroutine test_trapz_sp
 
     subroutine test_trapz_dp
@@ -59,26 +59,26 @@ contains
         integer :: i
 
         print *, "test_trapz_dp"
-        
+
         y = [(real(i-1, dp), i = 1, n)]
 
         val = trapz(y, 1.0_dp)
         ans = 128.0_dp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         val = trapz(y, 0.5_dp)
         ans = 64.0_dp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = [((i-1)*4.0_dp/real(n-1, dp), i = 1, n)]
         val = trapz(y, x)
         ans = 32.0_dp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         val = trapz(y, x)
         ans = 2728.0_sp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
     end subroutine test_trapz_dp
 
 
@@ -91,26 +91,26 @@ contains
         integer :: i
 
         print *, "test_trapz_qp"
-        
+
         y = [(real(i-1, qp), i = 1, n)]
 
         val = trapz(y, 1.0_qp)
         ans = 128.0_qp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         val = trapz(y, 0.5_qp)
         ans = 64.0_qp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = [((i-1)*4.0_qp/real(n-1, qp), i = 1, n)]
         val = trapz(y, x)
         ans = 32.0_qp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         val = trapz(y, x)
         ans = 2728.0_qp
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
     end subroutine test_trapz_qp
 
 
@@ -124,20 +124,20 @@ contains
         real(sp) :: ans
 
         print *, "test_trapz_weights_sp"
-        
+
         y = [(real(i-1, sp), i = 1, n)]
 
         x = y
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
     end subroutine test_trapz_weights_sp
 
@@ -152,24 +152,24 @@ contains
         real(dp) :: ans
 
         print *, "test_trapz_weights_dp"
-        
+
         y = [(real(i-1, dp), i = 1, n)]
 
         x = y
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
     end subroutine test_trapz_weights_dp
 
-    
+
     subroutine test_trapz_weights_qp
         integer, parameter :: n = 17
         real(qp), dimension(n) :: y
@@ -180,20 +180,20 @@ contains
         real(qp) :: ans
 
         print *, "test_trapz_weights_qp"
-        
+
         y = [(real(i-1, qp), i = 1, n)]
 
         x = y
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
         x = y**2
         w = trapz_weights(x)
         val = dot_product(w, y)
         ans = trapz(y, x)
-        call assert(val == ans)
+        call assert(abs(val - ans) < epsilon(ans))
 
     end subroutine test_trapz_weights_qp
 
@@ -202,11 +202,11 @@ contains
         real(sp), dimension(0) :: a
 
         print *, "test_trapz_zero_sp"
-        
-        call assert(trapz(a, 1.0_sp) == 0.0_sp)
-        call assert(trapz([1.0_sp], 1.0_sp) == 0.0_sp)
-        call assert(trapz(a, a) == 0.0_sp)
-        call assert(trapz([1.0_sp], [1.0_sp]) == 0.0_sp)
+
+        call assert(abs(trapz(a, 1.0_sp)) < epsilon(0.0_sp))
+        call assert(abs(trapz([1.0_sp], 1.0_sp)) < epsilon(0.0_sp))
+        call assert(abs(trapz(a, a)) < epsilon(0.0_sp))
+        call assert(abs(trapz([1.0_sp], [1.0_sp])) < epsilon(0.0_sp))
     end subroutine test_trapz_zero_sp
 
 
@@ -214,11 +214,11 @@ contains
         real(dp), dimension(0) :: a
 
         print *, "test_trapz_zero_dp"
-        
-        call assert(trapz(a, 1.0_dp) == 0.0_dp)
-        call assert(trapz([1.0_dp], 1.0_dp) == 0.0_dp)
-        call assert(trapz(a, a) == 0.0_dp)
-        call assert(trapz([1.0_dp], [1.0_dp]) == 0.0_dp)
+
+        call assert(trapz(a, 1.0_dp)) < epsilon(0.0_dp))
+        call assert(abs(trapz([1.0_dp], 1.0_dp)) < epsilon(0.0_dp))
+        call assert(abs(trapz(a, a)) < epsilon(0.0_dp))
+        call assert(abs(trapz([1.0_dp], [1.0_dp])) < epsilon(0.0_dp))
     end subroutine test_trapz_zero_dp
 
 
@@ -226,11 +226,11 @@ contains
         real(qp), dimension(0) :: a
 
         print *, "test_trapz_zero_qp"
-        
-        call assert(trapz(a, 1.0_qp) == 0.0_qp)
-        call assert(trapz([1.0_qp], 1.0_qp) == 0.0_qp)
-        call assert(trapz(a, a) == 0.0_qp)
-        call assert(trapz([1.0_qp], [1.0_qp]) == 0.0_qp)
+
+        call assert(trapz(a, 1.0_qp) < epsilon(0.0_qp))
+        call assert(trapz([1.0_qp], 1.0_qp) < epsilon(0.0_qp))
+        call assert(trapz(a, a) < epsilon(0.0_qp))
+        call assert(trapz([1.0_qp], [1.0_qp]) < epsilon(0.0_qp))
     end subroutine test_trapz_zero_qp
-    
+
 end program test_trapz
