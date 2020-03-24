cd30ca7c232fcbdfd5293c2cc29663008132a729
milancurcic <caomaco@gmail.com>
2020-03-24 18:08:08 -0400

    remove assert subroutine from stdlib_experimental_error

--- src/stdlib_experimental_error.f90
+++ src/stdlib_experimental_error.f90
@@ -5,32 +5,15 @@ implicit none
 private
 
 interface ! f{08,18}estop.f90
-module subroutine error_stop(msg, code)
-character(*), intent(in) :: msg
-integer, intent(in), optional :: code
-end subroutine error_stop
+    module subroutine error_stop(msg, code)
+        character(*), intent(in) :: msg
+        integer, intent(in), optional :: code
+    end subroutine error_stop
 end interface
 
-public :: assert, check, error_stop
+public :: check, error_stop
 
 contains
-
-subroutine assert(condition, code)
-! If condition == .false., it aborts the program.
-!
-! Arguments
-! ---------
-!
-logical, intent(in) :: condition
-integer, intent(in), optional :: code
-!
-! Example
-! -------
-!
-! call assert(a == 5)
-
-if (.not. condition) call error_stop("Assert failed.", code)
-end subroutine
 
 subroutine check(condition, msg, code, warn)
 
