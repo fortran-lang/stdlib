d6f9773d9f686ba9893c64adeb40322fd2ee2e0d
Aman-Godara <amangodara891@gmail.com>
2021-04-19 03:18:55 +0530

    Updated PR as per the feedback received

--- doc/specs/stdlib_math.md
+++ doc/specs/stdlib_math.md
@@ -19,8 +19,7 @@ title: math
 
 #### Description
 
-Limits the input value `x` to the given interval [`xmin`, `xmax`] (interval is `xmin` and `xmax` inclusive). Returns a value which lies in the given interval and is closest to the input value `x`.  
-If the input value `x` already lies in the given interval, then the output value will be equal to the input value.
+Returns a value which lies in the given interval [`xmin`, `xmax`] (interval is `xmin` and `xmax` inclusive) and is closest to the input value `x`.  
 
 #### Syntax
 
@@ -36,15 +35,15 @@ Elemental function.
 
 #### Argument(s)
 
-`x`: scalar of either `integer` or `real`. This argument is `intent(in)`.  
-`xmin`: scalar of either `integer` or `real`. This argument is `intent(in)`.  
-`xmax`: scalar of either `integer` or `real`, which must be greater than or equal to `xmin`. This argument is `intent(in)`.
+`x`: scalar of either `integer` or `real` type. This argument is `intent(in)`.  
+`xmin`: scalar of either `integer` or `real` type. This argument is `intent(in)`.  
+`xmax`: scalar of either `integer` or `real` type, which must be greater than or equal to `xmin`. This argument is `intent(in)`.
 
 Note: All arguments must have same `type` and same `kind`.
 
 #### Output value or Result value
 
-Output is a scalar of either `integer` or `real` depending on the arguments. The output value will have `type` and `kind` same as to that of the arguments.
+The output is a scalar of `type` and `kind` same as to that of the arguments.
 
 #### Examples
 
@@ -52,9 +51,9 @@ Output is a scalar of either `integer` or `real` depending on the arguments. The
 
 Here inputs are of type `integer` and kind `int32`
 ```fortran
-program demo
+program demo_clip_integer
   use stdlib_math
-  use iso_fortran_env
+  use stdlib_kinds
   implicit none
   integer(int32) :: x
   integer(int32) :: xmin
@@ -70,30 +69,30 @@ program demo
 
   clipped_value = clip(x, xmin, xmax)
   ! clipped_value <- 5
-end program demo
+end program demo_clip_integer
 ```
 
 ##### Example 2:
 
-Here inputs are of type `real` and kind `real32` (or `sp`)
+Here inputs are of type `real` and kind `sp`
 ```fortran
-program demo
+program demo_clip_real
   use stdlib_math
-  use iso_fortran_env
+  use stdlib_kinds
   implicit none
-  real(real32) :: x
-  real(real32) :: xmin
-  real(real32) :: xmax
-  real(real32) :: clipped_value
+  real(sp) :: x
+  real(sp) :: xmin
+  real(sp) :: xmax
+  real(sp) :: clipped_value
 
-  xmin = -5.769_real32
+  xmin = -5.769_sp
   ! xmin <-  -5.76900005
-  xmax = 3.025_real32
+  xmax = 3.025_sp
   ! xmax <- 3.02500010
-  x = 3.025_real32
+  x = 3.025_sp
   ! x <- 3.02500010
 
   clipped_value = clip(x, xmin, xmax)
   ! clipped_value <- 3.02500010
-end program demo
+end program demo_clip_real
 ```
--- src/tests/math/test_math.f90
+++ src/tests/math/test_math.f90
@@ -1,6 +1,5 @@
 ! SPDX-Identifier: MIT
 
-
 module test_math
     use stdlib_kinds, only: int8, int16, int32, int64, sp, dp, qp
     use stdlib_error, only : check
@@ -8,56 +7,56 @@ module test_math
 
     implicit none
 
-    contains
-        subroutine test_clip_integer_int8(x, xmin, xmax, compare)
-            integer(int8), intent(in) :: x, xmin, xmax, compare
+contains
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_integer_int8
+    subroutine test_clip_int8(x, xmin, xmax, compare)
+        integer(int8), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_integer_int16(x, xmin, xmax, compare)
-            integer(int16), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_int8
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_integer_int16
+    subroutine test_clip_int16(x, xmin, xmax, compare)
+        integer(int16), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_integer_int32(x, xmin, xmax, compare)
-            integer(int32), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_int16
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_integer_int32
+    subroutine test_clip_int32(x, xmin, xmax, compare)
+        integer(int32), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_integer_int64(x, xmin, xmax, compare)
-            integer(int64), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_int32
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_integer_int64
+    subroutine test_clip_int64(x, xmin, xmax, compare)
+        integer(int64), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_real_sp(x, xmin, xmax, compare)
-            real(sp), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_int64
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_real_sp
+    subroutine test_clip_sp(x, xmin, xmax, compare)
+        real(sp), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_real_dp(x, xmin, xmax, compare)
-            real(dp), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_sp
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_real_dp
+    subroutine test_clip_dp(x, xmin, xmax, compare)
+        real(dp), intent(in) :: x, xmin, xmax, compare
 
-        subroutine test_clip_real_qp(x, xmin, xmax, compare)
-            real(qp), intent(in) :: x, xmin, xmax, compare
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_dp
 
-            call check(clip(x, xmin, xmax) == compare)
-        
-        end subroutine test_clip_real_qp
+    subroutine test_clip_qp(x, xmin, xmax, compare)
+        real(qp), intent(in) :: x, xmin, xmax, compare
 
+        call check(clip(x, xmin, xmax) == compare)
+    
+    end subroutine test_clip_qp
 
 end module test_math
 
@@ -71,62 +70,60 @@ program tester
     ! invalid case: xmin is greater than xmax
 
 
+    ! type: integer(int8), kind: int8
+        ! valid test cases
+        call test_clip_int8(2_int8, -2_int8, 5_int8, 2_int8)
+        call test_clip_int8(127_int8, -127_int8, 0_int8, 0_int8)
+        ! invalid test cases
+        call test_clip_int8(2_int8, 5_int8, -2_int8, 5_int8)
+        call test_clip_int8(127_int8, 0_int8, -127_int8, 0_int8)
 
-        ! type: integer, kind: int8
-            ! valid test cases
-            call test_clip_integer_int8(2_int8, -2_int8, 5_int8, 2_int8)
-            call test_clip_integer_int8(127_int8, -127_int8, 0_int8, 0_int8)
+    ! type: integer(int16), kind: int16
+        ! valid test cases
+        call test_clip_int16(2_int16, -2_int16, 5_int16, 2_int16)
+        call test_clip_int16(32767_int16, -32767_int16, 0_int16, 0_int16)
+        ! invalid test cases
+        call test_clip_int16(2_int16, 5_int16, -2_int16, 5_int16)
+        call test_clip_int16(32767_int16, 0_int16, -32767_int16, 0_int16)
 
-            ! invalid test cases
-            call test_clip_integer_int8(2_int8, 5_int8, -2_int8, 5_int8)
-            call test_clip_integer_int8(127_int8, 0_int8, -127_int8, 0_int8)
-        ! type: integer, kind: int16
-            ! valid test cases
-            call test_clip_integer_int16(2_int16, -2_int16, 5_int16, 2_int16)
-            call test_clip_integer_int16(32767_int16, -32767_int16, 0_int16, 0_int16)
+    ! type: integer(int32), kind: int32
+        ! valid test cases
+        call test_clip_int32(2_int32, -2_int32, 5_int32, 2_int32)
+        call test_clip_int32(-2147483647_int32, 0_int32, 2147483647_int32, 0_int32)
+        ! invalid test cases
+        call test_clip_int32(2_int32, 5_int32, -2_int32, 5_int32)
+        call test_clip_int32(-2147483647_int32, 2147483647_int32, 0_int32, 2147483647_int32)
 
-            ! invalid test cases
-            call test_clip_integer_int16(2_int16, 5_int16, -2_int16, 5_int16)
-            call test_clip_integer_int16(32767_int16, 0_int16, -32767_int16, 0_int16)
-        ! type: integer, kind: int32
-            ! valid test cases
-            call test_clip_integer_int32(2_int32, -2_int32, 5_int32, 2_int32)
-            call test_clip_integer_int32(-2147483647_int32, 0_int32, 2147483647_int32, 0_int32)
+    ! type: integer(int64), kind: int64
+        ! valid test cases
+        call test_clip_int64(2_int64, -2_int64, 5_int64, 2_int64)
+        call test_clip_int64(-922337203_int64, -10_int64, 25_int64, -10_int64)
+        ! invalid test cases
+        call test_clip_int64(2_int64, 5_int64, -2_int64, 5_int64)
+        call test_clip_int64(-922337203_int64, 25_int64, -10_int64, 25_int64)
 
-            ! invalid test cases
-            call test_clip_integer_int32(2_int32, 5_int32, -2_int32, 5_int32)
-            call test_clip_integer_int32(-2147483647_int32, 2147483647_int32, 0_int32, 2147483647_int32)
-        ! type: integer, kind: int64
-            ! valid test cases
-            call test_clip_integer_int64(2_int64, -2_int64, 5_int64, 2_int64)
-            call test_clip_integer_int64(-922337203_int64, -10_int64, 25_int64, -10_int64)
+    ! type: real(sp), kind: sp
+        ! valid test cases
+        call test_clip_sp(3.025_sp, -5.77_sp, 3.025_sp, 3.025_sp)
+        call test_clip_sp(0.0_sp, -1578.025_sp, -59.68_sp, -59.68_sp)
+        ! invalid test cases
+        call test_clip_sp(3.025_sp, 3.025_sp, -5.77_sp, 3.025_sp)
+        call test_clip_sp(0.0_sp, -59.68_sp, -1578.025_sp, -59.68_sp)
 
-            ! invalid test cases
-            call test_clip_integer_int64(2_int64, 5_int64, -2_int64, 5_int64)
-            call test_clip_integer_int64(-922337203_int64, 25_int64, -10_int64, 25_int64)
-        ! type: real, kind: sp
-            ! valid test cases
-            call test_clip_real_sp(3.025_sp, -5.77_sp, 3.025_sp, 3.025_sp)
-            call test_clip_real_sp(0.0_sp, -1578.025_sp, -59.68_sp, -59.68_sp)
+    ! type: real(dp), kind: dp
+        ! valid test cases
+        call test_clip_dp(3.025_dp, -5.77_dp, 3.025_dp, 3.025_dp)
+        call test_clip_dp(-7.0_dp, 0.059668_dp, 1.00268_dp, 0.059668_dp)
+        ! invalid test cases
+        call test_clip_dp(3.025_dp, 3.025_dp, -5.77_dp, 3.025_dp)
+        call test_clip_dp(-7.0_dp, 1.00268_dp, 0.059668_dp, 1.00268_dp)
 
-            ! invalid test cases
-            call test_clip_real_sp(3.025_sp, 3.025_sp, -5.77_sp, 3.025_sp)
-            call test_clip_real_sp(0.0_sp, -59.68_sp, -1578.025_sp, -59.68_sp)
-        ! type: real, kind: dp
-            ! valid test cases
-            call test_clip_real_dp(3.025_dp, -5.77_dp, 3.025_dp, 3.025_dp)
-            call test_clip_real_dp(-7.0_dp, 0.059668_dp, 1.00268_dp, 0.059668_dp)
-
-            ! invalid test cases
-            call test_clip_real_dp(3.025_dp, 3.025_dp, -5.77_dp, 3.025_dp)
-            call test_clip_real_dp(-7.0_dp, 1.00268_dp, 0.059668_dp, 1.00268_dp)
-        ! type: real, kind: qp
-            ! valid test cases
-            call test_clip_real_qp(3.025_qp, -5.77_qp, 3.025_qp, 3.025_qp)
-            call test_clip_real_qp(-55891546.2_qp, -8958133457.23_qp, -689712245.23_qp, -689712245.23_qp)
-
-            ! invalid test cases
-            call test_clip_real_qp(3.025_qp, 3.025_qp, -5.77_qp, 3.025_qp)
-            call test_clip_real_qp(-55891546.2_qp, -689712245.23_qp, -8958133457.23_qp, -689712245.23_qp)
+    ! type: real(qp), kind: qp
+        ! valid test cases
+        call test_clip_qp(3.025_qp, -5.77_qp, 3.025_qp, 3.025_qp)
+        call test_clip_qp(-55891546.2_qp, -8958133457.23_qp, -689712245.23_qp, -689712245.23_qp)
+        ! invalid test cases
+        call test_clip_qp(3.025_qp, 3.025_qp, -5.77_qp, 3.025_qp)
+        call test_clip_qp(-55891546.2_qp, -689712245.23_qp, -8958133457.23_qp, -689712245.23_qp)
 
 end program tester
