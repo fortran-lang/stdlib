3040135ec5d7784d93a7a5aa494f2c524baa0692
Evan Voyles <ejovo13@yahoo.com>
2021-05-31 12:27:07 +0200

    Update test to include negative integer for elements requested

--- src/tests/stats/test_linspace.f90
+++ src/tests/stats/test_linspace.f90
@@ -4,10 +4,11 @@ program test_linspace
     use stdlib_stats, only: linspace 
 
     implicit none
-    logical :: warn = .true.
+    logical :: warn = .false.
 
     call test_linspace_sp
     call test_linspace_dp
+    call test_linspace_neg_index ! Make sure that when passed a negative index the result is an empty array
 
 contains
 
@@ -22,7 +23,8 @@ contains
         x = linspace(start, end, n)
 
         call check(x(1) == start, msg="Initial value of array is not equal to the passed start parameter", warn=warn)
-
+        call check(x(20) == end, msg="Final array value is not equal to end parameter", warn=warn)
+        call check(size(x) == n, msg="Array not allocated to appropriate size", warn=warn)
 
     end subroutine
 
@@ -36,6 +38,21 @@ contains
         x = linspace(start, end)
 
         call check(x(1) == start, msg="Initial value of array is not equal to the passed start parameter", warn=warn)
+        call check(x(100) == end, msg="Final array value is not equal to end parameter", warn=warn)
+        call check(size(x) == 100, msg="Array not allocated to appropriate size", warn=warn)
+
+    end subroutine
+
+    subroutine test_linspace_neg_index
+
+        real(dp) :: start = 1.0_dp
+        real(dp) :: end = 10.0_dp
+
+        real(dp), dimension(:), allocatable :: x
+
+        x = linspace(start, end, -15)
+
+        call check(size(x) == 0, msg="Allocated array is not empty", warn=warn)
 
     end subroutine
 
