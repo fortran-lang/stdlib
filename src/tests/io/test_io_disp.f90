df88c550cd26c474e885ae2810e5ec8e31554ef9
zoziha13 <1325686572@qq.com>
2021-06-28 00:18:50 +0800

    Fix stdlib_io_disp routines. --- - stdlib_io.fypp 1. add disp interface comments 2. remove `impure` label - stdlib_io_disp.fypp 1. remove `non_intrinsic` label 2. update some `format_string` to `to_string` 3. update `fmt_r` & `fmt_c` 4. add some routine comments 5. update the writing of some statements - update test_io_disp.f90 - notes 1. make passed 2. cmake passed

--- src/tests/io/test_io_disp.f90
+++ src/tests/io/test_io_disp.f90
@@ -1,15 +1,17 @@
 program test_io_disp
     use, non_intrinsic :: stdlib_io, only: disp
     implicit none
-    real :: r(2, 3)
+    real(8) :: r(2, 3)
     complex :: c(2, 3), c_3d(2, 3, 2)
     integer :: i(2, 3)
     logical :: l(2, 3)
 
     r = 1.; c = 1.; c_3d = 2.; i = 1; l = .true.
-    r(1, 1) = (1.e-11, 1.0e-4)
-    c(2, 2) = 10.e5
+    r(1, 1) = -1.e-11
+    r(1, 2) = -1.e10
+    c(2, 2) = (-1.e10,-1.e10)
     c_3d(1,3,1) = (1000, 0.001)
+    c_3d(1,3,2) = (1.e4, 100.)
         call disp('string', 'disp(string):')
         call disp('It is a note.')
         call disp()
