a77931c7815c97ca91cff4a256e9e3dc235516c2
Aman-Godara <amangodara891@gmail.com>
2021-07-16 20:23:22 +0530

    added test cases for move

--- src/tests/string/test_string_intrinsic.f90
+++ src/tests/string/test_string_intrinsic.f90
@@ -463,6 +463,40 @@ contains
         call check(code == iachar("F"))
     end subroutine test_iachar
 
+    subroutine test_move
+        type(string_type) :: from_string
+        type(string_type) :: to_string
+        character(len=:), allocatable :: from_char
+
+        from_string = "Move This String"
+        from_char = "Move This Char"
+        call check(from_string == "Move This String" .and. to_string == "" .and. &
+                    & from_char == "Move This Char", "move: test_case 1")
+
+        ! string_type (allocated) --> string_type (not allocated)
+        call move(from_string, to_string)
+        call check(from_string == "" .and. to_string == "Move This String", "move: test_case 2")
+
+        ! character (allocated) --> string_type (not allocated)
+        call move(from_char, from_string)
+        call check(.not. allocated(from_char) .and. from_string == "Move This Char", &
+                    & "move: test_case 3")
+
+        ! string_type (allocated) --> character (not allocated)
+        call move(to_string, from_char)
+        call check(to_string == "" .and. from_char == "Move This String", "move: test_case 4")
+
+        ! character (allocated) --> string_type (allocated)
+        call move(from_char, from_string)
+        call check(.not. allocated(from_char) .and. from_string == "Move This String", &
+                    & "move: test_case 5")
+
+        ! character (allocated) --> string_type (allocated)
+        call move(from_char, from_string)
+        call check(.not. allocated(from_char) .and. from_string == "", "move: test_case 6")
+
+    end subroutine test_move
+
 end module test_string_intrinsic
 
 program tester
@@ -485,5 +519,6 @@ program tester
     call test_char
     call test_ichar
     call test_iachar
+    call test_move
 
 end program tester
