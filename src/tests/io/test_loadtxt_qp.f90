2ef8724e21ed8930414fe1b52d0c65342578a46c
Milan Curcic <caomaco@gmail.com>
2021-08-30 15:23:52 -0400

    Port test_loadtxt_qp.f90 to test-drive

--- src/tests/io/test_loadtxt_qp.f90
+++ src/tests/io/test_loadtxt_qp.f90
@@ -1,30 +1,64 @@
-program test_loadtxt_qp
-use stdlib_kinds, only: qp
-use stdlib_io, only: loadtxt
-implicit none
+module test_loadtxt_qp
+    use stdlib_kinds, only: qp
+    use stdlib_io, only: loadtxt, savetxt
+    use stdlib_test, only: new_unittest, unittest_type, error_type, check
+    implicit none
 
-real(qp), allocatable :: q(:, :)
-
-call loadtxt("array4.dat", q)
-call print_array(q)
-
+    private
+    public :: collect_loadtxt_qp
 contains
 
-subroutine print_array(a)
-class(*),intent(in) :: a(:, :)
-integer :: i
-print *, "Array, shape=(", size(a, 1), ",", size(a, 2), ")"
+    !> Collect all exported unit tests
+    subroutine collect_loadtxt_qp(testsuite)
+        !> Collection of tests
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
- select type(a)
-  type is(real(qp))
-   do i = 1, size(a, 1)
-    print *, a(i, :)
-   end do
-  class default
-   write(*,'(a)')'The proposed type is not supported'
-   error stop
- end select
+        testsuite = [ &
+            new_unittest("loadtxt_qp", test_loadtxt_qp_) &
+        ]
 
-end subroutine
+    end subroutine collect_loadtxt_qp
 
-end program
+
+    subroutine test_loadtxt_qp_(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        real(qp), allocatable :: input(:,:), expected(:,:)
+
+        call loadtxt("array4.dat", input)
+        call savetxt("array4_new.dat", input)
+        call loadtxt("array4_new.dat", expected)
+        call check(error, all(input == expected))
+        if (.not. allocated(error)) return
+
+    end subroutine test_loadtxt_qp_
+
+end module test_loadtxt_qp
+
+
+program tester
+    use, intrinsic :: iso_fortran_env, only : error_unit
+    use stdlib_test, only : run_testsuite, new_testsuite, testsuite_type
+    use test_loadtxt_qp, only : collect_loadtxt_qp
+    implicit none
+    integer :: stat, is
+    type(testsuite_type), allocatable :: testsuites(:)
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+    stat = 0
+
+    testsuites = [ &
+        new_testsuite("loadtxt_qp", collect_loadtxt_qp) &
+        ]
+
+    do is = 1, size(testsuites)
+        write(error_unit, fmt) "Testing:", testsuites(is)%name
+        call run_testsuite(testsuites(is)%collect, error_unit, stat)
+    end do
+
+    if (stat > 0) then
+        write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+        error stop
+    end if
+
+end program tester
