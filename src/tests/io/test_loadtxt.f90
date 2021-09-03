9d54d5834cc27424f235b19dac2f0d5bbc79a4e5
Milan Curcic <caomaco@gmail.com>
2021-09-03 16:05:07 -0400

    Port stdlib_io tests to test-drive
    * Port test_open.f90 to test-drive
    * Port test_parse_mode.f90 to test-drive
    * Port test_savetxt.f90 to test-drive
    * Port test_savetxt_qp.f90 to test-drive
    * Port test_loadtxt.f90 to test-drive
    * Add complex data test to test_loadtxt.f90
    * Port test_loadtxt_qp.f90 to test-drive
    * Clean all test artifacts in Makefile.manual
    * Fix error handling in test_loadtxt
    * Don't reuse output file names between tests

--- src/tests/io/test_loadtxt.f90
+++ src/tests/io/test_loadtxt.f90
@@ -1,63 +1,127 @@
-program test_loadtxt
-use stdlib_kinds, only: int32, sp, dp
-use stdlib_io, only: loadtxt
-use stdlib_error, only: error_stop
-implicit none
+module test_loadtxt
+    use stdlib_kinds, only: int32, sp, dp
+    use stdlib_io, only: loadtxt, savetxt
+    use stdlib_test, only: new_unittest, unittest_type, error_type, check
+    implicit none
 
-integer(int32), allocatable :: i(:, :)
-real(sp), allocatable :: s(:, :)
-real(dp), allocatable :: d(:, :)
-complex(dp), allocatable :: z(:, :)
-
-call loadtxt("array1.dat", i)
-call print_array(i)
-
-call loadtxt("array1.dat", s)
-call print_array(s)
-
-call loadtxt("array1.dat", d)
-call print_array(d)
-
-call loadtxt("array2.dat", d)
-call print_array(d)
-
-call loadtxt("array3.dat", d)
-call print_array(d)
-
-call loadtxt("array4.dat", d)
-call print_array(d)
-
-call loadtxt("array5.dat", z)
-call print_array(z)
-
+    private
+    public :: collect_loadtxt
 contains
 
-subroutine print_array(a)
-class(*),intent(in) :: a(:, :)
-integer :: i
-print *, "Array, shape=(", size(a, 1), ",", size(a, 2), ")"
+    !> Collect all exported unit tests
+    subroutine collect_loadtxt(testsuite)
+        !> Collection of tests
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
- select type(a)
-  type is(integer(int32))
-   do i = 1, size(a, 1)
-    print *, a(i, :)
-   end do
-  type is(real(sp))
-   do i = 1, size(a, 1)
-    print *, a(i, :)
-   end do
-  type is(real(dp))
-   do i = 1, size(a, 1)
-    print *, a(i, :)
-   end do
-  type is(complex(dp))
-    do i = 1, size(a, 1)
-      print *, a(i, :)
+        testsuite = [ &
+            new_unittest("loadtxt_int32", test_loadtxt_int32), &
+            new_unittest("loadtxt_sp", test_loadtxt_sp), &
+            new_unittest("loadtxt_dp", test_loadtxt_dp), &
+            new_unittest("loadtxt_complex", test_loadtxt_complex) &
+        ]
+
+    end subroutine collect_loadtxt
+
+
+    subroutine test_loadtxt_int32(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        integer(int32), allocatable :: input(:,:), expected(:,:)
+
+        call loadtxt("array1.dat", input)
+        call savetxt("array1_new.dat", input)
+        call loadtxt("array1_new.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+        call loadtxt("array2.dat", input)
+        call savetxt("array2_new.dat", input)
+        call loadtxt("array2_new.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+    end subroutine test_loadtxt_int32
+
+
+    subroutine test_loadtxt_sp(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        real(sp), allocatable :: input(:,:), expected(:,:)
+
+        call loadtxt("array3.dat", input)
+        call savetxt("array3_sp.dat", input)
+        call loadtxt("array3_sp.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+        call loadtxt("array4.dat", input)
+        call savetxt("array4_sp.dat", input)
+        call loadtxt("array4_sp.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+    end subroutine test_loadtxt_sp
+
+
+    subroutine test_loadtxt_dp(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        real(dp), allocatable :: input(:,:), expected(:,:)
+
+        call loadtxt("array3.dat", input)
+        call savetxt("array3_dp.dat", input)
+        call loadtxt("array3_dp.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+        call loadtxt("array4.dat", input)
+        call savetxt("array4_dp.dat", input)
+        call loadtxt("array4_dp.dat", expected)
+        call check(error, all(input == expected))
+        if (allocated(error)) return
+
+    end subroutine test_loadtxt_dp
+
+
+    subroutine test_loadtxt_complex(error)
+      !> Error handling
+      type(error_type), allocatable, intent(out) :: error
+      complex(dp), allocatable :: input(:,:), expected(:,:)
+
+      call loadtxt("array5.dat", input)
+      call savetxt("array5_new.dat", input)
+      call loadtxt("array5_new.dat", expected)
+      call check(error, all(input == expected))
+      if (allocated(error)) return
+
+  end subroutine test_loadtxt_complex
+
+end module test_loadtxt
+
+
+program tester
+    use, intrinsic :: iso_fortran_env, only : error_unit
+    use stdlib_test, only : run_testsuite, new_testsuite, testsuite_type
+    use test_loadtxt, only : collect_loadtxt
+    implicit none
+    integer :: stat, is
+    type(testsuite_type), allocatable :: testsuites(:)
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+    stat = 0
+
+    testsuites = [ &
+        new_testsuite("loadtxt", collect_loadtxt) &
+        ]
+
+    do is = 1, size(testsuites)
+        write(error_unit, fmt) "Testing:", testsuites(is)%name
+        call run_testsuite(testsuites(is)%collect, error_unit, stat)
     end do
-  class default
-   call error_stop('The proposed type is not supported')
- end select
 
-end subroutine
+    if (stat > 0) then
+        write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+        error stop
+    end if
 
-end program
+end program tester
