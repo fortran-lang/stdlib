b81199e62bed0341845084361789f18f2885010d
zoziha <zuo.zhihua@qq.com>
2021-09-13 17:49:01 +0800

    add ./ci/fpm.toml and update README.md .

--- ci/fpm-deployment.sh
+++ ci/fpm-deployment.sh
@@ -3,26 +3,22 @@
 #
 set -ex
 
-SRCDIR="stdlib-master"
-DESTDIR="stdlib"
+DESTDIR="stdlib-fpm"
 
 FYFLAGS="-DMAXRANK=4"
 
 mkdir -p $DESTDIR/src
 mkdir -p $DESTDIR/test
 
-# Clean destination
-rm -rf $DESTDIR/src/*
-rm -rf $DESTDIR/test/*
-
 # Preprocess stdlib sources
-ls $SRCDIR/src/*.fypp | cut -f1 -d. | xargs -i{} fypp {}.fypp {}.f90 $FYFLAGS
+ls src/*.fypp | cut -f1 -d. | xargs -i{} fypp {}.fypp {}.f90 $FYFLAGS
 
 # Collect stdlib files
-find $SRCDIR/src -maxdepth 1 -iname "*.f90" -exec cp {} $DESTDIR/src/ \;
-find $SRCDIR/src/tests -name "test_*.f90" -exec cp {} $DESTDIR/test/ \;
-find $SRCDIR/src/tests -name "*.dat" -exec cp {} $DESTDIR/ \;
-cp $SRCDIR/LICENSE $DESTDIR/
+find src -maxdepth 1 -iname "*.f90" -exec cp {} $DESTDIR/src/ \;
+find src/tests -name "test_*.f90" -exec cp {} $DESTDIR/test/ \;
+find src/tests -name "*.dat" -exec cp {} $DESTDIR/ \;
+cp LICENSE $DESTDIR/
+cp ci/fpm.toml $DESTDIR/
 
 # Source file workarounds for fpm
 rm $DESTDIR/test/test_always_fail.f90
