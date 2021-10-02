984aa300f3ccb51d844ad4510536fa60b5f47999
milancurcic <caomaco@gmail.com>
2021-10-02 18:44:09 -0400

    Port test_distribution_PRNG to test-drive

--- src/tests/stats/test_distribution_PRNG.f90
+++ src/tests/stats/test_distribution_PRNG.f90
@@ -1,108 +1,135 @@
-program test_distribution_PRNG
-    use stdlib_error, only : check
+module test_stats_distribution_prng
     use stdlib_kinds, only: int8, int16, int32, int64
     use stdlib_stats_distribution_PRNG, only : random_seed, dist_rand
-
+    use stdlib_test, only: new_unittest, unittest_type, error_type, check
     implicit none
-    logical ::  warn = .true.
 
-    call test_random_seed
-    call test_random_rand_iint8
-    call test_random_rand_iint16
-    call test_random_rand_iint32
-    call test_random_rand_iint64
+    private
+    public :: collect_stats_distribution_prng
 
+contains
 
-    contains
+    !> Collect all exported unit tests
+    subroutine collect_stats_distribution_prng(testsuite)
+        !> Collection of tests
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-    subroutine test_random_seed
+        testsuite = [ &
+            new_unittest("random_seed", test_random_seed), &
+            new_unittest("random_rand_iint8", test_random_rand_iint8), &
+            new_unittest("random_rand_iint16", test_random_rand_iint8), &
+            new_unittest("random_rand_iint32", test_random_rand_iint8), &
+            new_unittest("random_rand_iint64", test_random_rand_iint8) &
+        ]
+
+    end subroutine collect_stats_distribution_prng
+
+    subroutine test_random_seed(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+
         integer :: put, get, res(5)
         integer :: ans(5) = [-1859553078, -1933696596, -642834430, &
                              1711399314,  1548311463]
         integer :: i
 
-        print *, ""
-        print *, "Test random_seed"
         put = 135792468
         do i = 1, 5
-            call random_seed(put,get)
+            call random_seed(put, get)
             res(i) = get
             put = get
         end do
-        call check(all(res == ans), msg="random seed test failed.",warn=warn)
+        call check(error, all(res == ans))
     end subroutine test_random_seed
 
-    subroutine test_random_rand_iint8
+    subroutine test_random_rand_iint8(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
+        integer(int8) :: res(5), ans(5) = [118, -15, -72, 101, 70]
 
-        integer(int8) :: res(5), ans(5)=[118, -15, -72, 101, 70]
-
-
-        print *, ""
-        print *, "Test random_rand with kind int8"
         put = 12345678
         call random_seed(put, get)
         do i = 1, 5
             res(i) = dist_rand(1_int8)
         end do
-        call check(all(res == ans), msg="random_rand with kind int8 test" &
-                                     //" failed.", warn=warn)
+        call check(error, all(res == ans))
     end subroutine test_random_rand_iint8
 
-    subroutine test_random_rand_iint16
+    subroutine test_random_rand_iint16(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
+        integer(int16) :: res(5), ans(5) = [30286, -3799, -18204, 25947, 18148]
 
-        integer(int16) :: res(5), ans(5)=[30286, -3799, -18204, 25947, 18148]
-
-
-        print *, ""
-        print *, "Test random_rand with kind int16"
         put = 12345678
         call random_seed(put, get)
         do i = 1, 5
             res(i) = dist_rand(1_int16)
         end do
-        call check(all(res == ans), msg="random_rand with kind int16 test" &
-                                     //" failed.", warn=warn)
+        call check(error, all(res == ans))
     end subroutine test_random_rand_iint16
 
-    subroutine test_random_rand_iint32
+    subroutine test_random_rand_iint32(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-
         integer(int32) :: res(5), ans(5)=[1984865646, -248954393, -1192993267, &
                                   1700514835, 1189401802]
 
-
-        print *, ""
-        print *, "Test random_rand with kind int32"
         put = 12345678
         call random_seed(put, get)
         do i = 1, 5
             res(i) = dist_rand(1_int32)
         end do
-        call check(all(res == ans), msg="random_rand with kind int32 test" &
-                                     //" failed.", warn=warn)
+        call check(error, all(res == ans))
     end subroutine test_random_rand_iint32
 
-    subroutine test_random_rand_iint64
+    subroutine test_random_rand_iint64(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-
         integer(int64) :: res(5), ans(5)=[8524933037632333570_int64, &
                                   -1069250973542918798_int64,        &
                                   -5123867065024149335_int64,        &
                                   7303655603304982073_int64,         &
                                   5108441843522503546_int64]
 
-
-        print *, ""
-        print *, "Test random_rand with kind int64"
         put = 12345678
         call random_seed(put, get)
         do i = 1, 5
             res(i) = dist_rand(1_int64)
         end do
-        call check(all(res == ans), msg="random_rand with kind int64 test" &
-                                     //" failed.", warn=warn)
+        call check(error, all(res == ans))
     end subroutine test_random_rand_iint64
 
-end program test_distribution_PRNG
+end module test_stats_distribution_prng
+
+
+program tester
+    use iso_fortran_env, only: error_unit
+    use stdlib_test, only: new_testsuite, run_testsuite, testsuite_type
+    use test_stats_distribution_prng, only: collect_stats_distribution_prng
+    implicit none
+
+    integer :: stat, is
+    type(testsuite_type), allocatable :: testsuites(:)
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+    stat = 0
+
+    testsuites = [ &
+        new_testsuite("stats_distribution_prng", collect_stats_distribution_prng) &
+        ]
+
+    do is = 1, size(testsuites)
+        write(error_unit, fmt) "Testing:", testsuites(is)%name
+        call run_testsuite(testsuites(is)%collect, error_unit, stat)
+    end do
+
+    if (stat > 0) then
+        write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+        error stop
+    end if
+
+end program tester
