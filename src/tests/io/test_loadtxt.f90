de74cd8142b950872a0a2250eb49ba6ef8898bc4
Sebastian Müller <mueller.seb@posteo.de>
2022-04-19 13:14:51 +0200

    Add test for skiprows in loadtxt

--- src/tests/io/test_loadtxt.f90
+++ src/tests/io/test_loadtxt.f90
@@ -19,6 +19,7 @@ contains
             new_unittest("loadtxt_sp_huge", test_loadtxt_sp_huge), &
             new_unittest("loadtxt_sp_tiny", test_loadtxt_sp_tiny), &
             new_unittest("loadtxt_dp", test_loadtxt_dp), &
+            new_unittest("loadtxt_dp_skip", test_loadtxt_dp_skip), &
             new_unittest("loadtxt_dp_huge", test_loadtxt_dp_huge), &
             new_unittest("loadtxt_dp_tiny", test_loadtxt_dp_tiny), &
             new_unittest("loadtxt_complex", test_loadtxt_complex) &
@@ -132,6 +133,27 @@ contains
         end do
 
     end subroutine test_loadtxt_dp
+
+
+    subroutine test_loadtxt_dp_skip(error)
+        !> Error handling
+        type(error_type), allocatable, intent(out) :: error
+        real(dp), allocatable :: input(:,:), expected(:,:)
+        integer :: n
+
+        allocate(input(10,10))
+
+        do n = 1, 5
+            call random_number(input)
+            input = input - 0.5
+            call savetxt('test_dp_skip.txt', input)
+            call loadtxt('test_dp_skip.txt', expected, skiprows=n)
+            call check(error, all(input(n+1:,:) == expected))
+            deallocate(expected)
+            if (allocated(error)) return
+        end do
+
+    end subroutine test_loadtxt_dp_skip
 
 
     subroutine test_loadtxt_dp_huge(error)
