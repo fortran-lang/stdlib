ec8d47cee4e74da7ce04a086f6f50a37bd81aab8
Jeremie Vandenplas <jeremie.vandenplas@gmail.com>
2022-06-22 16:20:43 +0200

    progress

--- test/example/strings/demo_padl.f90
+++ test/example/strings/demo_padl.f90
@@ -2,12 +2,12 @@ program demo_padl
     use stdlib_string_type, only: string_type, assignment(=)
     use stdlib_strings, only: padl
     implicit none
-    string_type :: string
+    type(string_type) :: string
 
     string = "left pad this string"
 ! string <-- "left pad this string"
 
-    print *, padl(string, 25, "$") ! "$$$$$left pad this string"
+    print '(a)', padl(string, 25, "$") ! "$$$$$left pad this string"
 
     string = padl(string, 25)
 ! string <-- "     left pad this string"
