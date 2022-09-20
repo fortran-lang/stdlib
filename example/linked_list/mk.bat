327c8c17887828ff9eb72c7650558ea8bb256dd8
arjenmarkus <arjen.markus895@gmail.com>
2022-09-20 19:01:19 +0200

    Rename the examples to avoid conflicts
    Rename the examples so that the names of the targets do not conflict with existing examples for other modules (notably in the strings subdirectory

--- example/linked_list/mk.bat
+++ example/linked_list/mk.bat
@@ -1,17 +1,17 @@
-rem gfortran -c stdlib_child_list.f90
-gfortran -c stdlib_linked_list.f90
+gfortran -c ../../src/stdlib_child_list.f90
+gfortran -c ../../src/stdlib_linked_list.f90
 rem gfortran -c linked_list_aux.f90
 
-rem gfortran -o example_size example_size.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_clear example_clear.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_get example_get.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_insert example_insert.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_replace example_replace.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_remove example_remove.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_push example_push.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_pop example_pop.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_reverse example_reverse.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_concat example_concat.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-rem gfortran -o example_absorb example_absorb.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-gfortran -o example_slice example_slice.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
-gfortran -o example_splice example_splice.f90 stdlib_linked_list.o stdlib_child_list.o linked_list_aux.o
+gfortran -o example_size example_size.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_clear example_clear.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_get example_get.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_insert example_insert.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_replace example_replace.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_remove example_remove.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_push example_push.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_pop example_pop.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_reverse example_reverse.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_concat example_concat.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_absorb example_absorb.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_slice example_slice.f90 stdlib_linked_list.o stdlib_child_list.o
+gfortran -o example_splice example_splice.f90 stdlib_linked_list.o stdlib_child_list.o
