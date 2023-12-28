b31023978dd24f288ba2d24e7285d85ad47f9109
arjenmarkus <arjen.markus895@gmail.com>
2023-12-28 15:42:46 +0100

    Incorporate the auxiliary routine directly
    Something went wrong with the included routine in the examples. So instead, simply copy the source code for it in all examples.

--- example/linked_list/example_linked_remove.f90
+++ example/linked_list/example_linked_remove.f90
@@ -30,6 +30,30 @@ program example_remove
     call print_list( list )
 
 contains
-include 'linked_list_aux.inc'
+!include 'linked_list_aux.inc'
+subroutine print_list( list )
+    type(linked_list_type), intent(in) :: list
+
+    integer           :: i
+    class(*), pointer :: list_item
+
+    do i = 1,list%size()
+        list_item => list%get(i)
+
+        select type( item => list_item )
+            type is (integer)
+                write(*,*) i, item, ' (integer)'
+
+            type is (real)
+                write(*,*) i, item, ' (real)'
+
+            type is (character(*))
+                write(*,*) i, ' >', item, '< (string)'
+
+            class default
+                write(*,*) i, ' (type unknown)'
+        end select
+    enddo
+end subroutine print_list
 
 end program example_remove
