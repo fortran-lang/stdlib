a90f25a34a346cf09b148522637abf6c6cee25fe
Federico Perini <federico.perini@gmail.com>
2024-04-02 20:01:01 +0200

    more explanation

--- example/linalg/example_state1.f90
+++ example/linalg/example_state1.f90
@@ -3,7 +3,8 @@ program example_state1
   implicit none
   type(linalg_state_type) :: err
 
-  ! Create a state flag
+  ! To create a state flag, we enter a state flag, followed by any list of variables 
+  ! that will be assembled into a formatted error message
   err = linalg_state_type(LINALG_VALUE_ERROR,'just an example with scalar ',&
                           'integer=',1,'real=',2.0,'complex=',(3.0,1.0),'and array ',[1,2,3],'inputs')
 
