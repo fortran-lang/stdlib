dc51fabe21249c68355e919bca45510eb4d16c9e
Milan Skocic <milan.skocic@icloud.com>
2024-05-08 07:35:42 +0200

    Update example and add CMakeLists.txt in constant folder.

--- example/constants/example_constants.f90
+++ example/constants/example_constants.f90
@@ -1,13 +1,25 @@
 program example_constants
     use stdlib_constants, only: c, PI=>PI_dp
     use stdlib_codata, only: alpha=>ALPHA_PARTICLE_ELECTRON_MASS_RATIO
-    use stdlib_codata, only: ATOMIC_UNIT_OF_1ST_HYPERPOLARIZABILITY
+    use stdlib_codata_type, only : to_real
+    use stdlib_kinds, only: dp, sp
 
+    ! Use most common physical constants defined as deouble precision reals
+    print *, "speed of light in vacuum= ", c
+
+    ! Use of mathematical constants such as PI
+    print *, "PI as double precision real= ", PI
+    
+    ! Use codata_constant type for evaluating the value to the desired precision
+    print *, "Value of alpha... evaluated to double precision=", alpha%to_real(1.0_dp)
+    print *, "Uncertainty of alpha... evaluated to double precision=", alpha%to_real(1.0_sp, .true.)
+    print *, "Value of alpha... evaluated to single precision=", alpha%to_real(1.0_sp)
+
+    ! Convert a codata constant to a real
+    print *, "Value of the alpha... evaluated to double precision=", to_real(alpha, 1.0_dp)
+
+
+    ! Print out codata constant attributes: name, value, uncertainty and unit
     call alpha%print()
-
-    print *, ATOMIC_UNIT_OF_1ST_HYPERPOLARIZABILITY%eval_value(1.0)
-
-    print *, c
-    print *, PI
 
 end program example_constants
