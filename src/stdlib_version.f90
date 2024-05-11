5a1eb37eb76c0eefb60988d3ef938a0120f43fd1
jvdp1 <jeremie.vandenplas@gmail.com>
2024-05-11 09:08:06 +0000

    Deploying to stdlib-fpm from @ fortran-lang/stdlib@0a00ced88dc5a1b7b6c510ab3d9ca06d1ea1fa30 🚀

--- src/stdlib_version.f90
+++ src/stdlib_version.f90
@@ -11,13 +11,13 @@ module stdlib_version
 
 
     !> String representation of the standard library version
-    character(len=*), parameter :: stdlib_version_string = "0.5.0"
+    character(len=*), parameter :: stdlib_version_string = "0.6.0"
 
     !> Major version number of the above standard library version
     integer, parameter :: stdlib_major = 0
 
     !> Minor version number of the above standard library version
-    integer, parameter :: stdlib_minor = 5
+    integer, parameter :: stdlib_minor = 6
 
     !> Patch version number of the above standard library version
     integer, parameter :: stdlib_patch = 0
