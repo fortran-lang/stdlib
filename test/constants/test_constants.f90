97a4c88a42582d2ca99af4cdf509c815a7e5d4d5
Milan Skocic <milan.skocic@icloud.com>
2024-05-19 07:03:02 +0200

    Update test/constants/test_constants.f90
    Co-authored-by: Jeremie Vandenplas <jeremie.vandenplas@gmail.com>

--- test/constants/test_constants.f90
+++ test/constants/test_constants.f90
@@ -255,7 +255,6 @@ subroutine test_U_AVOGADRO_CONSTANT(error)
 end subroutine
 
 subroutine test_U_BOLTZMANN_CONSTANT(error)
-    implicit none
     type(error_type), allocatable, intent(out) :: error 
     real(dp) :: value, expected, diff, fac
     fac = 1.0d0
