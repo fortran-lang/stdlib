5fea52f363bc55a6720a64ee43016c391bbae10b
Milan Skocic <milan.skocic@icloud.com>
2024-05-19 07:04:35 +0200

    Update test/constants/test_constants.f90
    Co-authored-by: Jeremie Vandenplas <jeremie.vandenplas@gmail.com>

--- test/constants/test_constants.f90
+++ test/constants/test_constants.f90
@@ -277,7 +277,6 @@ subroutine test_U_ELECTRON_VOLT(error)
 end subroutine
 
 subroutine test_U_ELEMENTARY_CHARGE(error)
-    implicit none
     type(error_type), allocatable, intent(out) :: error 
     real(dp) :: value, expected, diff, fac
     fac = 1.0d0
