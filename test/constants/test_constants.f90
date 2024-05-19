a79e01f7c01b4ae82c1d00b54b455e8ec3b7f950
Milan Skocic <milan.skocic@icloud.com>
2024-05-19 07:09:07 +0200

    Update test/constants/test_constants.f90
    Co-authored-by: Jeremie Vandenplas <jeremie.vandenplas@gmail.com>

--- test/constants/test_constants.f90
+++ test/constants/test_constants.f90
@@ -332,7 +332,6 @@ subroutine test_U_PLANCK_CONSTANT(error)
 end subroutine
 
 subroutine test_U_SPEED_OF_LIGHT(error)
-    implicit none
     type(error_type), allocatable, intent(out) :: error 
     real(dp) :: value, expected, diff, fac
     fac = 1.0d0
