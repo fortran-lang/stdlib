22f5f372a052b5b5ea62752b51851ccc1f83ca5a
minhqdao <hello@minhdao.de>
2024-08-07 02:32:22 +0530

    Add list files in zip and add tests

--- src/stdlib_io_zip.f90
+++ src/stdlib_io_zip.f90
@@ -2,7 +2,10 @@ module stdlib_io_zip
     implicit none
     private
 
-    public :: unzip
+    public :: list_files_in_zip, unzip
+
+    character(*), parameter :: temp_folder = 'temp'
+    character(*), parameter :: zip_contents_file = temp_folder//'/'//'zip_contents.txt'
 
     !> Contains extracted raw data from a zip file.
     type, public :: t_unzipped_bundle
@@ -20,30 +23,100 @@ module stdlib_io_zip
 
 contains
 
+    subroutine run(command, stat, msg)
+        character(len=*), intent(in) :: command
+        integer, intent(out), optional :: stat
+        character(len=:), allocatable, intent(out), optional :: msg
+
+        integer :: exitstat, cmdstat
+        character(len=256) :: cmdmsg
+
+        if (present(stat)) stat = 0
+        exitstat = 0; cmdstat = 0
+
+        call execute_command_line(command, exitstat=exitstat, cmdstat=cmdstat, cmdmsg=cmdmsg)
+        if (exitstat /= 0 .or. cmdstat /= 0) then
+            if (present(stat)) then
+                if (exitstat /= 0) then
+                    stat = exitstat
+                else
+                    stat = cmdstat
+                end if
+            end if
+            if (present(msg) .and. trim(adjustl(cmdmsg)) /= '') msg = cmdmsg
+        end if
+    end
+
+    subroutine list_files_in_zip(filename, stat, msg)
+        character(len=*), intent(in) :: filename
+        integer, intent(out), optional :: stat
+        character(len=:), allocatable, intent(out), optional :: msg
+
+        integer :: run_stat
+        character(:), allocatable :: err_msg
+
+        if (present(stat)) stat = 0
+        run_stat = 0
+
+        call run('rm -f '//zip_contents_file, run_stat, err_msg)
+        if (run_stat /= 0) then
+            if (present(stat)) stat = run_stat
+            if (present(msg)) then
+                if (allocated(err_msg)) then
+                    msg = "Error removing file '"//zip_contents_file//"': '"//err_msg//"'"
+                else
+                    msg = "Error removing file '"//zip_contents_file//"'."
+                end if
+            end if
+            return
+        end if
+
+        call run('mkdir -p '//temp_folder, run_stat, err_msg)
+        if (run_stat /= 0) then
+            if (present(stat)) stat = run_stat
+            if (present(msg)) then
+                if (allocated(err_msg)) then
+                    msg = "Error creating folder '"//temp_folder//"': '"//err_msg//"'"
+                else
+                    msg = "Error creating folder '"//temp_folder//"'."
+                end if
+            end if
+            return
+        end if
+
+        call run('unzip -l '//trim(filename)//' | sed ''1,3d;$d;$d'' | awk ''{print $4}'' > zip_contents.txt' &
+            //zip_contents_file, run_stat, err_msg)
+        if (run_stat /= 0) then
+            if (present(stat)) stat = run_stat
+            if (present(msg)) then
+                if (allocated(err_msg)) then
+                    msg = "Error listing contents of '"//filename//"': '"//err_msg//"'"
+                else
+                    msg = "Error listing contents of '"//filename//"'."
+                end if
+            end if
+        end if
+    end
+
     subroutine unzip(filename, iostat, iomsg)
         character(len=*), intent(in) :: filename
         integer, intent(out), optional :: iostat
         character(len=:), allocatable, intent(out), optional :: iomsg
 
-        integer :: exitstat, cmdstat
-        character(len=256) :: cmdmsg
+        integer :: stat
+        character(:), allocatable :: msg
 
-        exitstat = 0; cmdstat = 0
+        if (present(iostat)) iostat = 0
+        stat = 0
 
-        call execute_command_line('unzip '//filename, exitstat=exitstat, cmdstat=cmdstat, cmdmsg=cmdmsg)
-        if (exitstat /= 0 .or. cmdstat /= 0) then
-            if (present(iostat)) then
-                if (exitstat /= 0) then
-                    iostat = exitstat
-                else
-                    iostat = cmdstat
-                end if
-            end if
+        call run('unzip '//filename, stat, msg)
+        if (stat /= 0) then
+            if (present(iostat)) iostat = stat
             if (present(iomsg)) then
-                if (trim(adjustl(cmdmsg)) == '') then
-                    iomsg = "Error unzipping '"//filename//"'."
+                if (allocated(msg)) then
+                    iomsg = "Error unzipping '"//filename//"': '"//msg//"'"
                 else
-                    iomsg = "Error unzipping '"//filename//"'"//": '"//cmdmsg//"'"
+                    iomsg = "Error unzipping '"//filename//"'."
                 end if
             end if
         end if
--- test/io/test_zip.f90
+++ test/io/test_zip.f90
@@ -1,5 +1,5 @@
 module test_zip
-    use stdlib_io_zip, only : unzip
+    use stdlib_io_zip
     use testdrive, only : new_unittest, unittest_type, error_type, check
     implicit none
     private
@@ -17,7 +17,11 @@ contains
             new_unittest("npz_file_not_exists", npz_file_not_exists, should_fail=.true.), &
             new_unittest("npz_points_to_directory", npz_points_to_directory, should_fail=.true.), &
             new_unittest("npz_is_not_zip", npz_is_not_zip, should_fail=.true.), &
-            new_unittest("npz_empty_zip", npz_empty_zip, should_fail=.true.) &
+            new_unittest("npz_empty_zip", npz_empty_zip, should_fail=.true.), &
+            new_unittest("npz_list_file_not_exists", npz_list_file_not_exists, should_fail=.true.), &
+            new_unittest("npz_list_file_is_directory", npz_list_file_is_directory, should_fail=.true.), &
+            new_unittest("npz_list_file_not_zip", npz_list_file_not_zip, should_fail=.true.), &
+            new_unittest("npz_list_empty_zip", npz_list_empty_zip, should_fail=.true.) &
             ]
     end
 
@@ -67,6 +71,54 @@ contains
 
         call delete_file(filename)
     end
+
+    subroutine npz_list_file_not_exists(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        integer :: stat
+
+        call list_files_in_zip("nonexistent.npz", stat)
+        call check(error, stat, "Trying to list the contents of a non-existent npz file should fail.")
+    end
+
+    subroutine npz_list_file_is_directory(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        integer :: stat
+
+        call list_files_in_zip(".", stat)
+        call check(error, stat, "Listing of contents of a zip file that actually points towards a directory should fail.")
+    end
+
+    subroutine npz_list_file_not_zip(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        integer :: io, stat
+        character(*), parameter :: filename = "non_zip_file"
+
+        open(newunit=io, file=filename)
+        call list_files_in_zip(filename, stat)
+        call check(error, stat, "Listing the contents of a non-zip file should fail.")
+        close(io, status="delete")
+    end
+
+    subroutine npz_list_empty_zip(error)
+        type(error_type), allocatable, intent(out) :: error
+
+        integer :: io, stat
+        character(*), parameter :: filename = "empty.zip"
+        character(*), parameter:: binary_data = 'PK'//char(5)//char(6)//repeat(char(0), 18)
+
+        open (newunit=io, file=filename, form='unformatted', access='stream')
+        write (io) binary_data
+        close (io)
+
+        call list_files_in_zip(filename, stat)
+        call check(error, stat, "Listint the contents of an empty zip file should fail.")
+
+        call delete_file(filename)
+    end
+
 
     subroutine delete_file(filename)
         character(len=*), intent(in) :: filename
