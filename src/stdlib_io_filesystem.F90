0fecec474376e02f75affda9931d499f8d0c8995
Federico Perini <federico.perini@gmail.com>
2024-12-08 11:31:43 +0100

    add `io_filesystem` module

---
+++ src/stdlib_io_filesystem.F90
@@ -0,0 +1,11 @@
+! SPDX-Identifier: MIT
+
+!> Interaction with the filesystem.
+module stdlib_io_filesystem
+    use stdlib_string_type, only: string_type
+    implicit none
+    private
+
+contains
+
+end module stdlib_io_filesystem
