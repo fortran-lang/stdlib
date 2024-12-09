5ca70964f2968da541434263b3838d5a547f1d6b
Federico Perini <federico.perini@gmail.com>
2024-12-09 03:55:08 -0600

    system: implement `run`, `null_device`

--- src/stdlib_system.F90
+++ src/stdlib_system.F90
@@ -1,8 +1,17 @@
 module stdlib_system
 use, intrinsic :: iso_c_binding, only : c_int, c_long
+use stdlib_string_type, only: string_type, assignment(=)
+use stdlib_io, only: getfile
+use stdlib_error, only: error_stop, state_type
 implicit none
 private
 public :: sleep
+
+!! Sub-processing
+public :: run
+
+
+public :: null_device
 
 !! OS type inquiry
                     public :: OS_NAME
@@ -58,7 +67,7 @@ call winsleep(int(millisec, c_long))
 #else
 !! Linux, Unix, MacOS, MSYS2, ...
 ierr = usleep(int(millisec * 1000, c_int))
-if (ierr/=0) error stop 'problem with usleep() system call'
+if (ierr/=0) call error_stop('problem with usleep() system call')
 #endif
 
 
@@ -66,14 +75,22 @@ end subroutine sleep
 
 !> Determine the current OS type
 integer function OS_TYPE() result(os)
-    if (have_os) then 
-        os = OS_CURRENT
-    else
+    if (.not.have_os) then 
         OS_CURRENT = runtime_os()
-        have_os = .true.
-        os = OS_CURRENT
+        have_os = .true.        
     end if
+    os = OS_CURRENT
 end function OS_TYPE
+
+!> Return the file path of the null device.
+function null_device()
+    character(:), allocatable :: null_device
+    if (OS_TYPE()==OS_WINDOWS) then 
+        null_device = 'NUL'
+    else
+        null_device = '/dev/null'
+    end if
+end function null_device
 
 !> Determine the current OS type at runtime
 integer function runtime_os() result(os)
@@ -189,5 +206,61 @@ pure function OS_NAME(os)
         case default     ; OS_NAME =  "Unknown"
     end select
 end function OS_NAME
+
+! Run a syncronous command
+subroutine run(cmd,exit_state,command_state,stdout,stderr)
+    character(len=*), intent(in) :: cmd
+    integer, intent(out), optional :: exit_state,command_state
+    type(string_type), optional, intent(out) :: stdout
+    type(string_type), optional, intent(out) :: stderr
+
+    character(len=4096) :: iomsg
+    type(state_type) :: err
+    logical :: want_stdout, want_stderr
+    character(:), allocatable :: redirect_file
+    integer :: cstat, estat, fh, iostat
+    
+    want_stdout = present(stdout)
+    want_stderr = present(stderr)
+
+    if (want_stdout) then
+        ! Redirect output to a file
+        redirect_file = scratch_name()
+    else
+        redirect_file = null_device()
+    endif
+
+    ! Execute command        
+    call execute_command_line(cmd//" >"//redirect_file//" 2>&1", wait = .true., exitstat=estat,cmdstat=cstat,cmdmsg=iomsg)
+    
+    ! Retrieve stdout, stderr
+    if (want_stdout) stdout = getfile(redirect_file,delete=.true.)
+    if (want_stderr) stderr = trim(iomsg)
+
+    if (present(exit_state)) then
+        exit_state = estat
+    elseif (estat /= 0) then
+        call error_stop('Cannot run: '//cmd)
+    end if
+
+    if (present(command_state)) then 
+        command_state = cstat
+    elseif (cstat /= 0) then 
+        call error_stop('Command error: '//cmd)
+    endif
+    
+    contains
+    
+        ! Simple timestamp-based temporary name generation
+        function scratch_name() result(temp_filename)
+            character(:), allocatable :: temp_filename
+            character(len=10) :: timestamp,yyyymmdd
+
+            call date_and_time(date=yyyymmdd,time=timestamp)
+
+            temp_filename = 'tmp_' // yyyymmdd(1:8) //'_'// timestamp(1:6) // '_' // timestamp(8:10) // '.tmp'
+        end function scratch_name    
+    
+end subroutine run
 
 end module stdlib_system
