15689bc2f7a361260f5f2e07b20bc212413b4e39
Federico Perini <federico.perini@gmail.com>
2025-01-28 20:08:27 +0100

    Update example_process_1.f90

--- example/system/example_process_1.f90
+++ example/system/example_process_1.f90
@@ -1,13 +1,17 @@
 ! Process example 1: Run a Command Synchronously and Capture Output
 program run_sync
-    use stdlib_system, only: run, is_completed, process_type
+    use stdlib_system, only: run, is_completed, is_windows, process_type
     implicit none
 
     type(process_type) :: p
     logical :: completed
 
     ! Run a synchronous process to list directory contents
-    p = run("ls -l", want_stdout=.true.)
+    if (is_windows()) then
+        p = run("dir", want_stdout=.true.)
+    else
+        p = run("ls -l", want_stdout=.true.)
+    end if
 
     ! Check if the process is completed (should be true since wait=.true.)
     if (is_completed(p)) then
