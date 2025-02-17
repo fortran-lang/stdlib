a1cfcfff31f31cd7f2c2b3ad86a76e2343affcbf
perazz <federico.perini@gmail.com>
2025-02-17 07:24:05 +0000

    Deploying to stdlib-fpm-ilp64 from @ fortran-lang/stdlib@1fe21714e589c1f18b28de49aec314147aefc1cf 🚀

--- src/stdlib_kinds.f90
+++ src/stdlib_kinds.f90
@@ -3,10 +3,10 @@
 !> The specification of this module is available [here](../page/specs/stdlib_kinds.html).
 module stdlib_kinds
   use iso_fortran_env, only: int8, int16, int32, int64
-  use iso_c_binding, only: c_bool
+  use iso_c_binding, only: c_bool, c_char
   implicit none
   private
-  public :: sp, dp, xdp, qp, int8, int16, int32, int64, lk, c_bool
+  public :: sp, dp, xdp, qp, int8, int16, int32, int64, lk, c_bool, c_char
 
   !> Single precision real numbers
   integer, parameter :: sp = selected_real_kind(6)
