06ce7351996597b1b6ab8cf22e2d2454529a28a8
supritsj@Arch <supritsj05@gmail.com>
2025-03-15 19:58:15 +0530

    slightly better examples

--- example/intrinsics/example_matmul.f90
+++ example/intrinsics/example_matmul.f90
@@ -1,7 +1,19 @@
 program example_matmul
     use stdlib_intrinsics, only: stdlib_matmul
-    complex :: a(2,2)
-    a = reshape([(0, 0), (0, -1), (0, 1), (0, 0)], [2, 2]) ! pauli y-matrix
+    complex :: x(2, 2), y(2, 2)
+    real :: r1(50, 100), r2(100, 40), r3(40, 50)
+    real, allocatable :: res(:, :)
+    x = reshape([(0, 0), (1, 0), (1, 0), (0, 0)], [2, 2])
+    y = reshape([(0, 0), (0, -1), (0, 1), (0, 0)], [2, 2]) ! pauli y-matrix
 
-    print *, stdlib_matmul(a, a, a, a, a) ! should be sigma_y
+    print *, stdlib_matmul(y, y, y, y, y) ! should be y
+    print *, stdlib_matmul(x, x, y, x) ! should be -i x sigma_z
+
+    call random_seed()
+    call random_number(r1)
+    call random_number(r2)
+    call random_number(r3)
+
+    res = stdlib_matmul(r1, r2, r3) ! 50x50 matrix
+    print *, shape(res)
 end program example_matmul
