c9d25319f72d317eaa3aa2f66cf1badf61995b7b
Jean-Christophe <loiseau.jc@gmail.com>
2025-03-25 15:54:46 +0100

    Fix errors in examples.

--- example/specialmatrices/example_specialmatrices_dp_spmv.f90
+++ example/specialmatrices/example_specialmatrices_dp_spmv.f90
@@ -1,15 +1,16 @@
 program example_tridiagonal_matrix
-   use stdlib_constants, only: dp
+   use stdlib_linalg_constants, only: dp
    use stdlib_specialmatrices
    implicit none
 
    integer, parameter :: n = 5
    type(Tridiagonal_dp_type) :: A
-   real(dp) :: dl(n), dv(n), du(n)
+   real(dp) :: dl(n - 1), dv(n), du(n - 1)
    real(dp) :: x(n), y(n), y_dense(n)
+   integer :: i
 
    ! Create an arbitrary tridiagonal matrix.
-   dl = [(i, i=1, n - 1)]; dv = [(2*i, i=1, n)]; du = [(3*i, i=1, n)]
+   dl = [(i, i=1, n - 1)]; dv = [(2*i, i=1, n)]; du = [(3*i, i=1, n - 1)]
    A = Tridiagonal(dl, dv, du)
 
    ! Initialize vectors.
--- example/specialmatrices/example_tridiagonal_dp_type.f90
+++ example/specialmatrices/example_tridiagonal_dp_type.f90
@@ -1,11 +1,11 @@
 program example_tridiagonal_matrix
-   use stdlib_constants, only: dp
+   use stdlib_linalg_constants, only: dp
    use stdlib_specialmatrices
    implicit none
 
    integer, parameter :: n = 5
    type(Tridiagonal_dp_type) :: A
-   real(dp) :: dl(n), dv(n), du(n)
+   real(dp) :: dl(n - 1), dv(n), du(n - 1)
 
    ! Generate random tridiagonal elements.
    call random_number(dl)
