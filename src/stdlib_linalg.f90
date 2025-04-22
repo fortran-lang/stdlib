b01458ab08696fbad3c87e489337fe1dc051c7b1
zoziha <zuo.zhihua@qq.com>
2025-04-22 03:05:38 +0000

    Deploying to stdlib-fpm from @ fortran-lang/stdlib@d8fd7b21836d411a4c8019ce0c6c1804204c8fe4 🚀

--- src/stdlib_linalg.f90
+++ src/stdlib_linalg.f90
@@ -238,74 +238,74 @@ module stdlib_linalg
       !
       ! Vector to matrix
       !
-      module function diag_rsp(v) result(res)
+      module pure function diag_rsp(v) result(res)
         real(sp), intent(in) :: v(:)
         real(sp) :: res(size(v),size(v))
       end function diag_rsp
-      module function diag_rdp(v) result(res)
+      module pure function diag_rdp(v) result(res)
         real(dp), intent(in) :: v(:)
         real(dp) :: res(size(v),size(v))
       end function diag_rdp
-      module function diag_csp(v) result(res)
+      module pure function diag_csp(v) result(res)
         complex(sp), intent(in) :: v(:)
         complex(sp) :: res(size(v),size(v))
       end function diag_csp
-      module function diag_cdp(v) result(res)
+      module pure function diag_cdp(v) result(res)
         complex(dp), intent(in) :: v(:)
         complex(dp) :: res(size(v),size(v))
       end function diag_cdp
-      module function diag_iint8(v) result(res)
+      module pure function diag_iint8(v) result(res)
         integer(int8), intent(in) :: v(:)
         integer(int8) :: res(size(v),size(v))
       end function diag_iint8
-      module function diag_iint16(v) result(res)
+      module pure function diag_iint16(v) result(res)
         integer(int16), intent(in) :: v(:)
         integer(int16) :: res(size(v),size(v))
       end function diag_iint16
-      module function diag_iint32(v) result(res)
+      module pure function diag_iint32(v) result(res)
         integer(int32), intent(in) :: v(:)
         integer(int32) :: res(size(v),size(v))
       end function diag_iint32
-      module function diag_iint64(v) result(res)
+      module pure function diag_iint64(v) result(res)
         integer(int64), intent(in) :: v(:)
         integer(int64) :: res(size(v),size(v))
       end function diag_iint64
-      module function diag_rsp_k(v,k) result(res)
+      module pure function diag_rsp_k(v,k) result(res)
         real(sp), intent(in) :: v(:)
         integer, intent(in) :: k
         real(sp) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_rsp_k
-      module function diag_rdp_k(v,k) result(res)
+      module pure function diag_rdp_k(v,k) result(res)
         real(dp), intent(in) :: v(:)
         integer, intent(in) :: k
         real(dp) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_rdp_k
-      module function diag_csp_k(v,k) result(res)
+      module pure function diag_csp_k(v,k) result(res)
         complex(sp), intent(in) :: v(:)
         integer, intent(in) :: k
         complex(sp) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_csp_k
-      module function diag_cdp_k(v,k) result(res)
+      module pure function diag_cdp_k(v,k) result(res)
         complex(dp), intent(in) :: v(:)
         integer, intent(in) :: k
         complex(dp) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_cdp_k
-      module function diag_iint8_k(v,k) result(res)
+      module pure function diag_iint8_k(v,k) result(res)
         integer(int8), intent(in) :: v(:)
         integer, intent(in) :: k
         integer(int8) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_iint8_k
-      module function diag_iint16_k(v,k) result(res)
+      module pure function diag_iint16_k(v,k) result(res)
         integer(int16), intent(in) :: v(:)
         integer, intent(in) :: k
         integer(int16) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_iint16_k
-      module function diag_iint32_k(v,k) result(res)
+      module pure function diag_iint32_k(v,k) result(res)
         integer(int32), intent(in) :: v(:)
         integer, intent(in) :: k
         integer(int32) :: res(size(v)+abs(k),size(v)+abs(k))
       end function diag_iint32_k
-      module function diag_iint64_k(v,k) result(res)
+      module pure function diag_iint64_k(v,k) result(res)
         integer(int64), intent(in) :: v(:)
         integer, intent(in) :: k
         integer(int64) :: res(size(v)+abs(k),size(v)+abs(k))
@@ -314,74 +314,74 @@ module stdlib_linalg
       !
       ! Matrix to vector
       !
-      module function diag_rsp_mat(A) result(res)
+      module pure function diag_rsp_mat(A) result(res)
         real(sp), intent(in) :: A(:,:)
         real(sp) :: res(minval(shape(A)))
       end function diag_rsp_mat
-      module function diag_rdp_mat(A) result(res)
+      module pure function diag_rdp_mat(A) result(res)
         real(dp), intent(in) :: A(:,:)
         real(dp) :: res(minval(shape(A)))
       end function diag_rdp_mat
-      module function diag_csp_mat(A) result(res)
+      module pure function diag_csp_mat(A) result(res)
         complex(sp), intent(in) :: A(:,:)
         complex(sp) :: res(minval(shape(A)))
       end function diag_csp_mat
-      module function diag_cdp_mat(A) result(res)
+      module pure function diag_cdp_mat(A) result(res)
         complex(dp), intent(in) :: A(:,:)
         complex(dp) :: res(minval(shape(A)))
       end function diag_cdp_mat
-      module function diag_iint8_mat(A) result(res)
+      module pure function diag_iint8_mat(A) result(res)
         integer(int8), intent(in) :: A(:,:)
         integer(int8) :: res(minval(shape(A)))
       end function diag_iint8_mat
-      module function diag_iint16_mat(A) result(res)
+      module pure function diag_iint16_mat(A) result(res)
         integer(int16), intent(in) :: A(:,:)
         integer(int16) :: res(minval(shape(A)))
       end function diag_iint16_mat
-      module function diag_iint32_mat(A) result(res)
+      module pure function diag_iint32_mat(A) result(res)
         integer(int32), intent(in) :: A(:,:)
         integer(int32) :: res(minval(shape(A)))
       end function diag_iint32_mat
-      module function diag_iint64_mat(A) result(res)
+      module pure function diag_iint64_mat(A) result(res)
         integer(int64), intent(in) :: A(:,:)
         integer(int64) :: res(minval(shape(A)))
       end function diag_iint64_mat
-      module function diag_rsp_mat_k(A,k) result(res)
+      module pure function diag_rsp_mat_k(A,k) result(res)
         real(sp), intent(in) :: A(:,:)
         integer, intent(in) :: k
         real(sp) :: res(minval(shape(A))-abs(k))
       end function diag_rsp_mat_k
-      module function diag_rdp_mat_k(A,k) result(res)
+      module pure function diag_rdp_mat_k(A,k) result(res)
         real(dp), intent(in) :: A(:,:)
         integer, intent(in) :: k
         real(dp) :: res(minval(shape(A))-abs(k))
       end function diag_rdp_mat_k
-      module function diag_csp_mat_k(A,k) result(res)
+      module pure function diag_csp_mat_k(A,k) result(res)
         complex(sp), intent(in) :: A(:,:)
         integer, intent(in) :: k
         complex(sp) :: res(minval(shape(A))-abs(k))
       end function diag_csp_mat_k
-      module function diag_cdp_mat_k(A,k) result(res)
+      module pure function diag_cdp_mat_k(A,k) result(res)
         complex(dp), intent(in) :: A(:,:)
         integer, intent(in) :: k
         complex(dp) :: res(minval(shape(A))-abs(k))
       end function diag_cdp_mat_k
-      module function diag_iint8_mat_k(A,k) result(res)
+      module pure function diag_iint8_mat_k(A,k) result(res)
         integer(int8), intent(in) :: A(:,:)
         integer, intent(in) :: k
         integer(int8) :: res(minval(shape(A))-abs(k))
       end function diag_iint8_mat_k
-      module function diag_iint16_mat_k(A,k) result(res)
+      module pure function diag_iint16_mat_k(A,k) result(res)
         integer(int16), intent(in) :: A(:,:)
         integer, intent(in) :: k
         integer(int16) :: res(minval(shape(A))-abs(k))
       end function diag_iint16_mat_k
-      module function diag_iint32_mat_k(A,k) result(res)
+      module pure function diag_iint32_mat_k(A,k) result(res)
         integer(int32), intent(in) :: A(:,:)
         integer, intent(in) :: k
         integer(int32) :: res(minval(shape(A))-abs(k))
       end function diag_iint32_mat_k
-      module function diag_iint64_mat_k(A,k) result(res)
+      module pure function diag_iint64_mat_k(A,k) result(res)
         integer(int64), intent(in) :: A(:,:)
         integer, intent(in) :: k
         integer(int64) :: res(minval(shape(A))-abs(k))
