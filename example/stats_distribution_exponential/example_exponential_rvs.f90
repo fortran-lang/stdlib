43f88eb9fd7c2f143069ec3019e25e4bd9ddf023
Sebastian G. Mutz <sebastian@mutz.science>
2025-05-08 11:51:50 +0200

    modified parameterisation for stats exp dist. procedures

--- example/stats_distribution_exponential/example_exponential_rvs.f90
+++ example/stats_distribution_exponential/example_exponential_rvs.f90
@@ -3,30 +3,28 @@ program example_exponential_rvs
   use stdlib_stats_distribution_exponential, only: rexp => rvs_exp
 
   implicit none
-  complex :: scale
+  complex :: cloc, cscale
   integer :: seed_put, seed_get
 
   seed_put = 1234567
   call random_seed(seed_put, seed_get)
 
   print *, rexp()         !single standard exponential random variate
-
 ! 0.358690143
 
-  print *, rexp(2.0)       !exponential random variate with lambda=2.0
+  print *, rexp(0.6, 0.2) !exponential random variate with loc=0.6 and scale=0.5 (lambda=2)
+! 0.681645989
 
-! 0.816459715
+  print *, rexp(0.0, 3.0, 10)   !an array of 10 variates with loc=0.0 and scale=3.0 (lambda=1/3)
+! 0.184008643      0.359742016       1.36567295       2.62772131      0.362352759
+! 5.47133636       2.13591909      0.410784155       5.83882189       6.71128035
 
-  print *, rexp(0.3, 10)   !an array of 10 variates with lambda=0.3
+  cloc   = (0.0, 0.0)
+  cscale = (0.5, 1.6)
+  print *, rexp(cloc, cscale)
+!single complex exponential random variate with real part of scale=0.5 (lambda=2.0);
+!imagainary part of scale=1.6 (lambda=0.625)
 
-!  1.84008647E-02  3.59742008E-02  0.136567295  0.262772143  3.62352766E-02
-!  0.547133625  0.213591918  4.10784185E-02  0.583882213  0.671128035
-
-  scale = (2.0, 0.7)
-  print *, rexp(scale)
-!single complex exponential random variate with real part of lambda=2.0;
-!imagainary part of lambda=0.7
-
-! (1.41435969,4.081114382E-02)
+! (0.219550118,1.01847279)
 
 end program example_exponential_rvs
