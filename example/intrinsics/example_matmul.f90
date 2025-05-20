a052599dfa49ad9d80fc302a73dbeb14f150957a
supritsj@Arch <supritsj05@gmail.com>
2025-05-20 16:33:32 +0530

    modified example and slight changes

--- example/intrinsics/example_matmul.f90
+++ example/intrinsics/example_matmul.f90
@@ -1,19 +1,12 @@
 program example_matmul
     use stdlib_intrinsics, only: stdlib_matmul
-    complex :: x(2, 2), y(2, 2)
-    real :: r1(50, 100), r2(100, 40), r3(40, 50)
-    real, allocatable :: res(:, :)
+    complex :: x(2, 2), y(2, 2), z(2, 2)
     x = reshape([(0, 0), (1, 0), (1, 0), (0, 0)], [2, 2])
     y = reshape([(0, 0), (0, 1), (0, -1), (0, 0)], [2, 2]) ! pauli y-matrix
+    z = reshape([(1, 0), (0, 0), (0, 0), (-1, 0)], [2, 2])
 
-    print *, stdlib_matmul(y, y, y) ! should be y
-    print *, stdlib_matmul(x, x, y, x) ! should be -i x sigma_z
-
-    call random_seed()
-    call random_number(r1)
-    call random_number(r2)
-    call random_number(r3)
-
-    res = stdlib_matmul(r1, r2, r3) ! 50x50 matrix
-    print *, shape(res)
+    print *, stdlib_matmul(x, y) ! should be iota*z
+    print *, stdlib_matmul(y, z, x) ! should be iota*identity
+    print *, stdlib_matmul(x, x, z, y) ! should be -iota*x
+    print *, stdlib_matmul(x, x, z, y, y) ! should be z
 end program example_matmul
