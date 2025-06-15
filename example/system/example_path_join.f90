ca867f8134ac32352f92d92c57d70b23c759e8d7
supritsj@Arch <supritsj05@gmail.com>
2025-06-16 01:13:59 +0530

    fix example_path_join.f90

--- example/system/example_path_join.f90
+++ example/system/example_path_join.f90
@@ -7,16 +7,17 @@ program example_path_join
     if( ISWIN ) then
         p1 = 'C:'/'Users'/'User1'/'Desktop'
         p2 = joinpath('C:\Users\User1', 'Desktop')
+        parr = [character(len=20) :: 'C:', 'Users', 'User1', 'Desktop']
+        p3 = joinpath(parr)
     else
         p1 = ''/'home'/'User1'/'Desktop'
         p2 = joinpath('/home/User1', 'Desktop')
+        parr = [character(len=20) :: '', 'home', 'User1', 'Desktop']
+        p3 = joinpath(parr)
     end if
 
-    parr = [character(len=20) :: '', 'home', 'User1', 'Desktop']
-    p3 = joinpath(parr)
-
-    ! (p1 == p2 == p3) = '/home/User1/Desktop' OR 'C:'/'Users'/'User1'/'Desktop'
-    print *, p1 ! /home/User1/Desktop OR 'C:'/'Users'/'User1'/'Desktop'
+    ! (p1 == p2 == p3) = '/home/User1/Desktop' OR 'C:\Users\User1\Desktop'
+    print *, p1 ! /home/User1/Desktop OR 'C:\Users\User1\Desktop'
     print *, "p1 == p2: ", p1 == p2 ! T
     print *, "p2 == p3: ", p2 == p3 ! T
 end program example_path_join
