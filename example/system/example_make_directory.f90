81339e70469c3067f1499f40f9801a50a9c69396
supritsj@Arch <supritsj05@gmail.com>
2025-07-23 02:43:32 +0530

    add example

--- example/system/example_make_directory.f90
+++ example/system/example_make_directory.f90
@@ -1,6 +1,6 @@
-! Illustrate the usage of `make_directory`
+! Illustrate the usage of `make_directory`, `make_directory_all`
 program example_make_directory
-    use stdlib_system, only: make_directory
+    use stdlib_system, only: make_directory, make_directory_all
     use stdlib_error, only: state_type
     implicit none
 
@@ -12,6 +12,14 @@ program example_make_directory
         print *, err%print()
     else
         print *, "directory created sucessfully"
+    end if
+
+    call make_directory_all("d1/d2/d3/d4", err)
+
+    if (err%error()) then
+        print *, err%print()
+    else
+        print *, "nested directories created sucessfully"
     end if
 
 end program example_make_directory
