b13edf5f305238e63bc39e3df465ee0288b89ed3
supritsj@Arch <supritsj05@gmail.com>
2025-07-24 22:42:42 +0530

    add in-code comments

--- src/stdlib_system.c
+++ src/stdlib_system.c
@@ -9,12 +9,16 @@
 #include <unistd.h>
 #endif /* ifdef _WIN32 */
 
+// Returns the string describing the meaning of `errno` code (by calling `strerror`).
 char* stdlib_strerror(size_t* len){
     char* err = strerror(errno);
     *len = strlen(err);
     return err;
 }
 
+// Wrapper to the platform's `mkdir`(make directory) call.
+// Uses `mkdir` on unix, `_mkdir` on windows.
+// Returns 0 if successful, otherwise returns the `errno`.
 int stdlib_make_directory(const char* path){
     int code;
 #ifdef _WIN32
@@ -27,6 +31,9 @@ int stdlib_make_directory(const char* path){
     return (!code) ? 0 : errno;
 }
 
+// Wrapper to the platform's `rmdir`(remove directory) call.
+// Uses `rmdir` on unix, `_rmdir` on windows.
+// Returns 0 if successful, otherwise returns the `errno`.
 int stdlib_remove_directory(const char* path){
     int code;
 #ifdef _WIN32
