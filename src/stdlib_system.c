8510867f7bf47cba149a4522280330ab64a6394d
Suprit S Jahagirdar <supritsj.d@gmail.com>
2025-08-03 00:29:37 +0530

    add C interfaces

--- src/stdlib_system.c
+++ src/stdlib_system.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -5,12 +6,39 @@
 #include <errno.h>
 #ifdef _WIN32
 #include <direct.h>
+#include <windows.h>
 #else
 #include <unistd.h>
 #endif /* ifdef _WIN32 */
 
-// Returns the string describing the meaning of `errno` code (by calling `strerror`).
-char* stdlib_strerror(size_t* len){
+// Wrapper to get the string describing a system syscall error.
+// Uses `strerr` on unix.
+// if `winapi` is `false`, uses the usual `strerr` on windows.
+// If `winapi` is `false`, uses `FormatMessage`(from windows.h) on windows.
+char* stdlib_strerror(size_t* len, bool winapi){
+
+    if (winapi) {
+#ifdef _WIN32
+    LPSTR err = NULL;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+    FORMAT_MESSAGE_ALLOCATE_BUFFER |
+    FORMAT_MESSAGE_FROM_SYSTEM |
+    FORMAT_MESSAGE_IGNORE_INSERTS,
+    NULL,
+    dw,
+    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+    (LPSTR) &err,
+    0,
+    NULL);
+
+    *len = strlen(err);
+    return (char*) err;
+
+#endif /* ifdef _WIN32 */
+    }
+
     char* err = strerror(errno);
     *len = strlen(err);
     return err;
@@ -43,4 +71,49 @@ int stdlib_remove_directory(const char* path){
 #endif /* ifdef _WIN32 */
 
     return (!code) ? 0 : errno;
+}
+
+// Wrapper to the platform's `stat`(status of path) call.
+// Uses `lstat` on unix, `GetFileAttributesA` on windows.
+// Returns the `type` of the path, and sets the `stat`(if any errors).
+int stdlib_exists(const char* path, int* stat){
+    // All the valid types
+    const int type_unknown = 0;
+    const int type_regular_file = 1;
+    const int type_directory = 2;
+    const int type_symlink = 3;
+
+    int type = type_unknown;
+    *stat = 0;
+
+#ifdef _WIN32
+    DWORD attrs = GetFileAttributesA(path);
+
+    if (attrs == INVALID_FILE_ATTRIBUTES) {
+        *stat = (int) GetLastError();
+        return type_unknown;
+    }
+
+    if (attrs & FILE_ATTRIBUTE_NORMAL) type = type_regular_file;
+    if (attrs & FILE_ATTRIBUTE_REPARSE_POINT) type = type_symlink;
+    if (attrs & FILE_ATTRIBUTE_DIRECTORY) type = type_directory;
+#else
+    struct stat buf = {0};
+    int status;
+    status = lstat(path, &buf);
+
+    if (status == -1) {
+        // `lstat` failed
+        *stat = errno;
+        return type_unknown;
+    }
+
+    switch (buf.st_mode & S_IFMT) {
+        case S_IFREG: type = type_regular_file; break;
+        case S_IFDIR: type = type_directory;    break;
+        case S_IFLNK: type = type_symlink;      break;
+        default:      type = type_unknown;      break;
+    }
+#endif /* ifdef _WIN32 */
+    return type;
 }
