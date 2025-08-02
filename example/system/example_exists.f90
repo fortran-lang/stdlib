866fddbae949ec97095e0fcca79b7d7247343b52
Suprit S Jahagirdar <supritsj.d@gmail.com>
2025-08-03 00:29:58 +0530

    added examples

---
+++ example/system/example_exists.f90
@@ -0,0 +1,26 @@
+! Illustrate the usage of `exists`
+program example_exists
+    use stdlib_system, only: exists, type_unknown, type_regular_file, &
+        type_directory, type_symlink
+    use stdlib_error, only: state_type
+    implicit none
+
+    type(state_type) :: err
+
+    character(*), parameter :: path = "path"
+    integer :: t
+
+    t = exists(path, err)
+
+    if (err%error()) then
+        print *, err%print()
+    end if
+
+    select case (t)
+    case (type_unknown); print *, "Unknown type!"
+    case (type_regular_file); print *, "Regular File!"
+    case (type_directory); print *, "Directory!"
+    case (type_symlink); print *, "Symbolic Link!"
+    end select
+end program example_exists
+
