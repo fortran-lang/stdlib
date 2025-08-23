d047e1fa703bd4b92cf46276d605bbb8b6e448c4
Suprit S Jahagirdar <supritsj.d@gmail.com>
2025-08-23 17:05:50 +0530

    add err handling to example

--- example/system/example_get_file_size.f90
+++ example/system/example_get_file_size.f90
@@ -1,10 +1,21 @@
 program example_get_file_size
+    use, intrinsic :: iso_fortran_env, only: int64
     use stdlib_system, only: get_file_size
+    use stdlib_error, only: state_type
     implicit none
-    
-    character(*), parameter :: path = "src"
 
-    print *, get_file_size(path)
+    type(state_type) :: err
+    integer(int64) :: size
+    
+    character(*), parameter :: path = "path/to/check"
+
+    size = get_file_size(path, err)
+
+    if (err%ok()) then
+        print *, "Size in bytes = ", size
+    else
+        print *, "Error!: ", err%print()
+    end if
 
 end program example_get_file_size
 
