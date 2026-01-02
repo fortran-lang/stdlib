9cd8e51e85200759abc023d86e527af24fa96341
Juan Fiol <fiol@cab.cnea.gov.ar>
2026-01-02 13:06:08 -0300

    Fixed typo and added short example with headings

--- example/io/example_savetxt.f90
+++ example/io/example_savetxt.f90
@@ -4,4 +4,6 @@ program example_savetxt
   real :: x(3, 2) = 1
   call savetxt('example.dat', x)
   call savetxt('example.csv', x, delimiter=',')
+  call savetxt('example1.dat', x, header='x  (x-units)   y  (y-units)')
+  call savetxt('example2.dat', x, header='x  (x-units)   y  (y-units)', comments='! ')
 end program example_savetxt
