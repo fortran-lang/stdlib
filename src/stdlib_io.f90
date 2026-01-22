e6ffb7ef35cf0ea0745b154eb6c994ad676a98f8
jvdp1 <jeremie.vandenplas@gmail.com>
2026-01-22 22:36:26 +0000

    Deploying to stdlib-fpm from @ fortran-lang/stdlib@f43e6f16d0ba24aa0b99d405111610187c632530 🚀

--- src/stdlib_io.f90
+++ src/stdlib_io.f90
@@ -7,6 +7,8 @@ module stdlib_io
   use, intrinsic :: iso_fortran_env, only : input_unit
   use stdlib_kinds, only: sp, dp, xdp, qp, &
       int8, int16, int32, int64
+  use stdlib_io_aux, only: FMT_INT, FMT_REAL_SP, FMT_REAL_DP, FMT_REAL_XDP, FMT_REAL_QP, &
+                                 FMT_COMPLEX_SP, FMT_COMPLEX_DP, FMT_COMPLEX_XDP, FMT_COMPLEX_QP
   use stdlib_error, only: error_stop, state_type, STDLIB_IO_ERROR
   use stdlib_optval, only: optval
   use stdlib_ascii, only: is_blank
@@ -41,29 +43,6 @@ module stdlib_io
   ! Private API that is exposed so that we can test it in tests
   public :: parse_mode
 
-  !> Version: experimental
-  !>
-  !> Format strings with edit descriptors for each type and kind
-  !> ([Specification](../page/specs/stdlib_io.html))
-  character(*), parameter :: &
-    !> Format string for integers
-    FMT_INT = '(i0)', &
-    !> Format string for single precision real numbers
-    FMT_REAL_SP = '(es15.8e2)', &
-    !> Format string for souble precision real numbers
-    FMT_REAL_DP = '(es24.16e3)', &
-    !> Format string for extended double precision real numbers
-    FMT_REAL_XDP = '(es26.18e3)', &
-    !> Format string for quadruple precision real numbers
-    FMT_REAL_QP = '(es44.35e4)', &
-    !> Format string for single precision complex numbers
-    FMT_COMPLEX_SP = '(es15.08e2,1x,es15.08e2)', &
-    !> Format string for double precision complex numbers
-    FMT_COMPLEX_DP = '(es24.16e3,1x,es24.16e3)', &
-    !> Format string for extended double precision complex numbers
-    FMT_COMPLEX_XDP = '(es26.18e3,1x,es26.18e3)', &
-    !> Format string for quadruple precision complex numbers
-    FMT_COMPLEX_QP = '(es44.35e4,1x,es44.35e4)'
   !> Default delimiter for loadtxt, savetxt and number_of_columns
   character(len=1), parameter :: delimiter_default = " "
 
