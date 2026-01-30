4f248b7a0f8d42132d9aed19432cd980443e36d4
Juan Fiol <fiol@cab.cnea.gov.ar>
2026-01-30 16:31:49 -0300

    test_headfoot is failing now

--- example/io/example_savetxt.f90
+++ example/io/example_savetxt.f90
@@ -6,7 +6,7 @@ program example_savetxt
   call savetxt('example.dat', x)
   call savetxt('example.csv', x, delimiter=',')
   call savetxt('example1.dat', x, header='x  (x-units)   y  (y-units)')
-  call savetxt('example2.dat', x, header='x  (x-units)   y  (y-units)', comments='! ', footer='This is all data')
+  call savetxt('example2.dat', x, header='x  (x-units)   y  (y-units)', comments='!#', footer='This is all data')
   call savetxt('example3.dat', x, fmt='g0.7')
   call savetxt(output_unit, x, header='x  (x-units)   y  (y-units)')
 end program example_savetxt
