23d31c5e703e085d86620eae13a74c9c4e3010a1
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2026-02-06 16:04:32 +0000

    Updated example structure (pdf)

--- example/stats_distribution_gamma/example_gamma_pdf.f90
+++ example/stats_distribution_gamma/example_gamma_pdf.f90
@@ -4,8 +4,8 @@ program example_gamma_pdf
                                                gamma_pdf => pdf_gamma
 
   implicit none
-  real :: x(2, 3, 4), g(2, 3, 4), s(2, 3, 4)
-  complex :: shape, rate
+  real :: x(2, 3, 4), loc(2, 3, 4), shape(2, 3, 4), rate(2, 3, 4)
+  complex :: cloc, cshape, crate
   integer :: seed_put, seed_get
 
   seed_put = 1234567
@@ -28,23 +28,25 @@ program example_gamma_pdf
   ! NaN
 
   ! gamma random variates array with loc=0.0, shape=2.0, rate=1.0
-  g(:, :, :) = 2.0
-  s(:, :, :) = 1.0
+  loc(:, :, :) = 0.0
+  shape(:, :, :) = 2.0
+  rate(:, :, :) = 1.0
   x = reshape(rgamma(0.0, 2.0, 1.0, 24), [2, 3, 4])
 
   ! a rank 3 gamma probability density array
-  print *, gamma_pdf(x, 0.0, g, s)
+  print *, gamma_pdf(x, loc, shape, rate)
   ! 0.204550430  0.320178866  0.274986655  0.348611295  0.101865448
   ! 0.102199331  0.358981341  0.223676488  0.254329354  0.356714427
   ! 0.267390072  0.305148095  0.367848188  7.26194456E-02  1.49471285E-02
   ! 0.246272027  0.360770017  0.339665830  0.101558588  0.358678699
   ! 0.224196941  0.359253854  7.56355673E-02  0.251869917
 
-  shape = (1.0, 1.5)
-  rate = (1.0, 2.0)
+  cloc = (0.0, 0.0)
+  cshape = (1.0, 1.5)
+  crate = (1.0, 2.0)
   ! complex gamma probability density function at (1.5, 1.0) with real part
   ! of shape=1.0, rate=1.0 and imaginary part of shape=1.5, rate=2.0
-  print *, gamma_pdf((1.5, 1.0), (0.0, 0.0), shape, rate)
+  print *, gamma_pdf((1.5, 1.0), cloc, cshape, crate)
   ! 9.63761061E-02
 
 end program example_gamma_pdf
