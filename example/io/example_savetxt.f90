ca904c7c80215b40017ecae46f5a9380e0a93a6b
Juan Fiol <fiol@cab.cnea.gov.ar>
2026-02-24 14:35:07 -0300

    Added checks for open file and writable unit

--- example/io/example_savetxt.f90
+++ example/io/example_savetxt.f90
@@ -10,10 +10,21 @@ program example_savetxt
   call savetxt('example.csv', x, delimiter=',')
   call savetxt('example1.dat', x, header='x  (x-units)   y  (y-units)')
   call savetxt('example2.dat', x, header='x  (x-units)   y  (y-units)', comments='!#', footer='This is all data')
-  call savetxt('example3.dat', x, fmt='g0.7')
   !
-  ! Output to standard output
-  call savetxt(output_unit, x, header='x  (x-units)   y  (y-units)')
+  ! Output to standard output. Custom format
+  call savetxt('example3.dat', x, fmt='f4.2')
+  ! Output
+  ! 1.00 4.00
+  ! 2.00 5.00
+  ! 3.00 6.00
+  !
+  ! Output to standard output. Diferent formats for the columns
+  call savetxt(output_unit, x, header='x   y', fmt='(f4.2,1x,f3.1)')
+  ! Output:
+  ! # x   y
+  ! 1.00 4.0
+  ! 2.00 5.0
+  ! 3.00 6.0
   !
   ! Save data and then overwrite it:
   call savetxt('example4.dat', x)
@@ -25,7 +36,7 @@ program example_savetxt
 
   ! Save data and then append some more:
   call savetxt('example5.dat', x, header='x  (x-units)   y  (y-units)')
-  call savetxt('example5.dat', 2*x, append=.True.)
+  call savetxt('example5.dat', 2 * x, append=.True.)
   ! Result in example5.dat:
   ! # x  (x-units)   y  (y-units)
   !  1.00000000E+00  4.00000000E+00
@@ -36,10 +47,10 @@ program example_savetxt
   !  6.00000000E+00  1.20000000E+01
 
   ! Save data and then append some more:
-  open(newunit=unit, file='example6.dat')
+  open (newunit=unit, file='example6.dat')
   call savetxt(unit, x, header='x  (x-units)   y  (y-units)')
-  call savetxt(unit, 2*x)
-  close(unit)
+  call savetxt(unit, 2 * x)
+  close (unit)
   ! Result in example6.dat:
   ! # x  (x-units)   y  (y-units)
   !  1.00000000E+00  4.00000000E+00
