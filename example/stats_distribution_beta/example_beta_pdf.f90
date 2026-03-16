50f7ba33eb290ccf636def2c24fbea9836a0e5bb
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2026-03-16 14:00:47 +0000

    applied same as previous change to cdf

--- example/stats_distribution_beta/example_beta_pdf.f90
+++ example/stats_distribution_beta/example_beta_pdf.f90
@@ -4,31 +4,26 @@ program example_beta_pdf
                                              beta_pdf => pdf_beta
 
   implicit none
-  real :: x, a, b
-  real :: xarr(3, 4, 5)
+  real, parameter :: a = 2.0, b = 5.0
+  real :: xarr(2, 5)
   integer :: seed_put, seed_get
 
   seed_put = 1234567
   call random_seed(seed_put, seed_get)
 
-  a = 2.0; b = 5.0
-
   ! probability density at x=0.3 for beta(2,5) distribution
   print *, beta_pdf(0.3, a, b)
-  ! 1.32859993
+  ! 2.16089988
 
   ! probability density at x=1.3 with loc=1.0 for beta(2,5) distribution
   print *, beta_pdf(1.3, a, b, 1.0)
-  ! 1.32859993
+  ! 2.16090035
 
   ! generate random variates and compute their pdf
-  xarr = reshape(rbeta(a, b, 60), [3, 4, 5])
+  xarr = reshape(rbeta(a, b, 10), [2, 5])
 
   print *, beta_pdf(xarr, a, b)
-  ! 1.23914337  0.925268888  0.969913423  0.998693407  1.44889069
-  ! 0.331506640  0.988773048  1.41048801  0.502468705  0.564825535
-  ! 0.661424160  0.931534827  0.861649513  1.01826906  1.44227338
-  ! 0.782453180  0.145308748  0.841068327  1.49033546  0.727149904
-  ! 1.14145494  0.905850768  1.08809102  1.40226245  ... (60 elements total)
+  ! 1.85633695  2.04246974  2.04407597  2.16859674  1.47261345
+  ! 1.67244565  2.07803488  0.819388986  2.38697886  1.08206940
 
 end program example_beta_pdf
