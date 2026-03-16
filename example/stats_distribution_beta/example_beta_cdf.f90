9940c0ac0d1c7d6d7ae877974b6a047baf859397
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2026-03-16 13:47:06 +0000

    tweaked beta cdf example
    made a and b paramters and reduced the elements

--- example/stats_distribution_beta/example_beta_cdf.f90
+++ example/stats_distribution_beta/example_beta_cdf.f90
@@ -4,14 +4,12 @@ program example_beta_cdf
                                              beta_cdf => cdf_beta
 
   implicit none
-  real :: x, a, b
-  real :: xarr(3, 4, 5)
+  real, parameter :: a = 2.0, b = 5.0
+  real :: xarr(2, 5)
   integer :: seed_put, seed_get
 
   seed_put = 1234567
   call random_seed(seed_put, seed_get)
-
-  a = 2.0; b = 5.0
 
   ! cumulative probability at x=0.3 for beta(2,5) distribution
   print *, beta_cdf(0.3, a, b)
@@ -22,13 +20,10 @@ program example_beta_cdf
   ! 0.471808970
 
   ! generate random variates and compute their cdf
-  xarr = reshape(rbeta(a, b, 60), [3, 4, 5])
+  xarr = reshape(rbeta(a, b, 10), [2, 5])
 
   print *, beta_cdf(xarr, a, b)
   ! 0.374293357  0.136472717  0.153627276  0.166885555  0.535913110
   ! 4.47619371E-02  0.161991328  0.524897814  7.37934634E-02  8.41872990E-02
-  ! 0.102836564  0.138294637  0.122145072  0.171486780  0.532901883
-  ! 0.111447386  1.87034653E-02  0.119697235  0.550687969  9.98932496E-02
-  ! 0.234867483  0.132088020  0.210879743  0.520989060  ... (60 elements total)
 
 end program example_beta_cdf
