22ed21ca984a60059c7b4b21b2efb807bb7cc648
Mahmood-Sinan <sinanmahmood7@gmail.com>
2026-04-24 11:23:03 +0530

    example modified

--- example/io/example_matrix_market.f90
+++ example/io/example_matrix_market.f90
@@ -5,14 +5,12 @@ program example_matrix_market
 
     real(dp), allocatable :: matrix(:,:), matrix2(:,:)
     integer, allocatable  :: index(:,:)
-    real(dp), allocatable :: data(:)
+    complex(dp), allocatable :: data(:)
     character(len=*), parameter :: dense_filename = "example_dense.mtx"
     character(len=*), parameter :: sparse_filename = "example_sparse.mtx"
     integer :: iostat, i
     character(len=:), allocatable :: iomsg
 
-    iostat = 0
-    iomsg = ''
     ! Create a test dense matrix
     allocate(matrix(3,3))
     matrix = reshape([1.0_dp, 2.0_dp, 3.0_dp, &
@@ -24,7 +22,7 @@ program example_matrix_market
     call print_matrix(matrix)
 
     ! Save dense matrix to Matrix Market file
-    call save_mm(dense_filename, matrix, iostat=iostat, iomsg=iomsg)
+    call save_mm(dense_filename, matrix, format="ES24.15E2", symmetry="general", iostat=iostat, iomsg=iomsg)
     if (iostat /= 0) then
         print *, "Error saving dense matrix: ", iomsg
         stop 1
@@ -42,11 +40,25 @@ program example_matrix_market
     print *, "Loaded dense matrix:"
     call print_matrix(matrix2)
 
-    ! Create a sparse test file manually for demonstration
-    call create_sparse_test_file(sparse_filename)
-
     print *, "=== Sparse Matrix Example ==="
-    print *, "Loading sparse matrix from ", sparse_filename
+    ! Create a test sparse matrix
+    allocate(index(2,6))
+    allocate(data(6))
+    index(:,1) = [1,1]; data(1) = (10.0_dp, -1.5_dp)
+    index(:,2) = [2,2]; data(2) = (20.0_dp, 2.0_dp)
+    index(:,3) = [3,3]; data(3) = (30.0_dp, 3.0_dp)
+    index(:,4) = [4,4]; data(4) = (40.0_dp, -4.0_dp)
+    index(:,5) = [1,4]; data(5) = ( 5.0_dp, -7.5_dp)
+    index(:,6) = [3,1]; data(6) = (15.0_dp, 25.0_dp)
+
+    ! Save sparse matrix to Matrix Market file
+    call save_mm(sparse_filename, index, data, format="ES24.15E2", symmetry="general", iostat=iostat, iomsg=iomsg)
+    if (iostat /= 0) then
+        print *, "Error saving sparse matrix: ", iomsg
+        stop 1
+    end if
+
+    print *, "Sparse matrix saved to ", sparse_filename
 
     ! Load sparse matrix from Matrix Market file
     call load_mm(sparse_filename, index, data, iostat=iostat, iomsg=iomsg)
@@ -72,22 +84,5 @@ contains
         end do
         print *
     end subroutine print_matrix
-
-    subroutine create_sparse_test_file(filename)
-        character(len=*), intent(in) :: filename
-        integer :: u
-        
-        open(newunit=u, file=filename, status='replace')
-        write(u, '(A)') '%%MatrixMarket matrix coordinate real general'
-        write(u, '(A)') '% This is a test sparse matrix'
-        write(u, '(A)') '4 4 6'
-        write(u, '(A)') '1 1 10.0'
-        write(u, '(A)') '2 2 20.0'
-        write(u, '(A)') '3 3 30.0'
-        write(u, '(A)') '4 4 40.0'
-        write(u, '(A)') '1 4 5.0'
-        write(u, '(A)') '3 1 15.0'
-        close(u)
-    end subroutine create_sparse_test_file
 
 end program example_matrix_market
