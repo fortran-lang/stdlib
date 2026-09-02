e8162feab6080b579c969095e332f0a3436289b4
José Alves <102541118+jalvesz@users.noreply.github.com>
2026-09-02 20:31:39 +0200

    Update parameter declarations in test_random.f90

--- test/stats/test_random.f90
+++ test/stats/test_random.f90
@@ -30,7 +30,7 @@ contains
         type(error_type), allocatable, intent(out) :: error
 
         integer :: put, get, res(5)
-        integer :: ans(5) = [-1859553078, -1933696596, -642834430, &
+        integer, parameter :: ans(5) = [-1859553078, -1933696596, -642834430, &
                              1711399314,  1548311463]
         integer :: i
 
@@ -47,7 +47,8 @@ contains
         !> Error handling
         type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-        integer(int8) :: res(5), ans(5) = [118, -15, -72, 101, 70]
+        integer(int8) :: res(5)
+        integer(int8), parameter :: ans(5) = [118, -15, -72, 101, 70]
 
         put = 12345678
         call random_seed(put, get)
@@ -61,7 +62,8 @@ contains
         !> Error handling
         type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-        integer(int16) :: res(5), ans(5) = [30286, -3799, -18204, 25947, 18148]
+        integer(int16) :: res(5)
+        integer(int16), parameter :: ans(5) = [30286, -3799, -18204, 25947, 18148]
 
         put = 12345678
         call random_seed(put, get)
@@ -75,7 +77,8 @@ contains
         !> Error handling
         type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-        integer(int32) :: res(5), ans(5)=[1984865646, -248954393, -1192993267, &
+        integer(int32) :: res(5)
+        integer(int32), parameter :: ans(5)=[1984865646, -248954393, -1192993267, &
                                   1700514835, 1189401802]
 
         put = 12345678
@@ -90,7 +93,8 @@ contains
         !> Error handling
         type(error_type), allocatable, intent(out) :: error
         integer :: put, get, i
-        integer(int64) :: res(5), ans(5)=[8524933037632333570_int64, &
+        integer(int64) :: res(5)
+        integer(int64), parameter :: ans(5)=[8524933037632333570_int64, &
                                   -1069250973542918798_int64,        &
                                   -5123867065024149335_int64,        &
                                   7303655603304982073_int64,         &
